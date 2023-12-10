da407aea064c58d81d474d5fff573aaa20d29fe9
Joe Wezorek <jwezorek@gmail.com>
2023-12-10 08:26:00 -0800

    simplified

--- src/2023/day_10.cpp
+++ src/2023/day_10.cpp
@@ -20,10 +20,21 @@ namespace {
         int row;
     };
 
+    loc operator*(int k, const loc& p) {
+        return { k * p.col, k * p.row };
+    }
+
     loc operator+(const loc& lhs, const loc& rhs) {
         return {
             lhs.col + rhs.col,
             lhs.row + rhs.row
+        };
+    }
+
+    loc operator-(const loc& lhs, const loc& rhs) {
+        return {
+            lhs.col - rhs.col,
+            lhs.row - rhs.row
         };
     }
 
@@ -32,6 +43,10 @@ namespace {
             return lhs.col == rhs.col && lhs.row == rhs.row;
         }
     };
+
+    bool operator==(const loc& lhs, const loc& rhs) {
+        return loc_equal()(lhs, rhs);
+    }
 
     struct loc_hash {
         size_t operator()(const loc& loc) const {
@@ -47,165 +62,145 @@ namespace {
     template<typename T>
     using loc_map = std::unordered_map<loc, T, loc_hash, loc_equal>;
 
-    bool operator==(const loc& lhs, const loc& rhs) {
-        return loc_equal()(lhs, rhs);
-    }
 
-    using cell = std::array<loc, 2>;
 
-    cell cell_from_char(const loc& cell_loc, char ch) {
+    std::array<loc, 2> neighbor_offsets(char ch) {
         if (ch == '.' || ch == 'S') {
             return { loc{-1,-1}, loc{-1,-1} };
         }
-        static const std::unordered_map<char, cell> char_to_offsets = {
-            {'J', { loc{0,-1}, loc{-1,0}}},
+        static const std::unordered_map<char, std::array<loc, 2>> char_to_offsets = {
+            {'J', { loc{-1,0}, loc{0,-1} }},
             {'|', { loc{0,-1}, loc{0,1}}},
             {'-', { loc{-1,0}, loc{1,0}}},
             {'L', { loc{0,-1}, loc{1,0}}},
             {'7', { loc{-1,0}, loc{0,1}}},
-            {'F', { loc{1,0}, loc{0,1}}}
+            {'F', { loc{0,1}, loc{1,0}, }}
         };
-        const auto& offsets = char_to_offsets.at(ch);
+        return char_to_offsets.at(ch);
+    }
+
+    std::array<loc, 2> neighbors(const std::vector<std::string>& grid, const loc& loc) {
+        auto offsets = neighbor_offsets(grid[loc.row][loc.col]);
         return {
-            cell_loc + offsets[0],
-            cell_loc + offsets[1]
+            loc + offsets.front(),
+            loc + offsets.back()
         };
     }
 
-    cell make_empty_cell() {
-        return { loc{-1,-1}, loc{-1,-1} };
-    }
-
-    cell fill_in_start_cell(const std::vector<std::vector<cell>>& grid, const loc& start) {
-        int i = 0;
+    char get_start_tile(const std::vector<std::string>& grid, const loc& start) {
         int rows = static_cast<int>(grid.size());
         int cols = static_cast<int>(grid.front().size());
-        static const std::array<loc, 4> neighbors = { loc{0,-1}, loc{1,0}, loc{0,1}, loc{-1,0} };
-        cell output;
-        for (const auto& offset : neighbors) {
-            auto neighbor = start + offset;
-            if (neighbor.row < 0 || neighbor.col < 0 ||
-                    neighbor.row >= rows || neighbor.col >= cols) {
+        static const std::array<loc, 4> neighbor_offsets = { loc{-1,0}, loc{0,-1}, loc{0,1}, loc{1,0} };
+        int i = 0;
+        std::array<loc, 2> neighbors;
+        for (const auto& offset : neighbor_offsets) {
+            auto adjacent = start + offset;
+            if (adjacent.row < 0 || adjacent.col < 0 ||
+                adjacent.row >= rows || adjacent.col >= cols) {
                 continue;
             }
-            for (auto neighbor_neighbor : grid[neighbor.row][neighbor.col]) {
-                if (neighbor_neighbor == start) {
-                    output[i++] = neighbor;
+            for (auto adj_adj : ::neighbors(grid, adjacent)) {
+                if (adj_adj == start) {
+                    neighbors[i++] = adjacent - start;
                 }
             }
+        }
+        static const loc_map<char> discr_to_tile{
+            {{-2, -1}, 'J'}, {{0,-1}, '|'}, {{-1,0}, '-'},
+            {{1,-1}, 'L'}, {{-2,1}, '7'}, {{1,2}, 'F'}
+        };
+        return discr_to_tile.at(2 * neighbors.front() + neighbors.back());
+    }
+
+    std::tuple< loc, std::vector<std::string>> parse_input(const std::vector<std::string>& input) {
+        auto grid = input;
+        loc start;
+        for (const auto& [y, row] : rv::enumerate(input)) {
+            for (const auto& [x, col] : rv::enumerate(row)) {
+                if (col == 'S') {
+                    start = { static_cast<int>(x), static_cast<int>(y) };
+                }
+            }
+        }
+        grid[start.row][start.col] = get_start_tile(grid, start);
+        return { start, std::move(grid) };
+    }
+
+    std::optional<loc> move_cursor(const loc_set& visited, const std::vector<std::string>& grid, 
+            const loc& loc) {
+        for (const auto& neighbor : neighbors( grid, loc)) {
+            if (!visited.contains(neighbor)) {
+                return neighbor;
+            }
+        }
+        return {};
+    }
+
+    int do_part_1(const std::vector<std::string>& grid, const loc& start) {
+        loc_set visited;
+        const auto& start_neighbors = neighbors(grid, start);
+        loc cursor_1 = start_neighbors.front();
+        loc cursor_2 = start_neighbors.back();
+        visited.insert(start);
+        visited.insert(cursor_1);
+        visited.insert(cursor_2);
+        int dist = 1;
+        while (cursor_1 != cursor_2) {
+            cursor_1 = *move_cursor(visited, grid, cursor_1);
+            cursor_2 = *move_cursor(visited, grid, cursor_2);
+            ++dist;
+            visited.insert(cursor_1);
+            visited.insert(cursor_2);
+        }
+        return dist;
+    }
+
+    loc_set get_loop(const std::vector<std::string>& grid, const loc& start) {
+        loc_set visited;
+        loc cursor = start;
+
+        do {
+            visited.insert(cursor);
+            auto next = move_cursor(visited, grid, cursor);
+            cursor = (next) ? *next : start;
+        } while (cursor != start);
+
+        return visited;
+    }
+
+    std::vector<std::string> paint_loop(const loc_set& loop, 
+            const std::vector<std::string>& grid) {
+        int rows = static_cast<int>(grid.size());
+        int cols = static_cast<int>(grid.front().size());
+        std::vector<std::string> output(rows, std::string(cols, '.'));
+        for (const auto& p : loop) {
+            output[p.row][p.col] = grid[p.row][p.col];
         }
         return output;
     }
 
-    std::tuple<loc, std::vector<std::vector<cell>>> parse_input(const std::vector<std::string>& lines) {
-        int rows = static_cast<int>(lines.size());
-        int cols = static_cast<int>(lines.front().size());
-
-        loc start;
-        std::vector<std::vector<cell>> grid(rows, std::vector<cell>(cols, make_empty_cell()));
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                grid[row][col] = cell_from_char({ col,row }, lines.at(row).at(col));
-                if (lines.at(row).at(col) == 'S') {
-                    start = { col,row };
-                }
+    int count_interior_of_row(const std::string& row) {
+        int wall_count = 0;
+        int interior_count = 0;
+        for (char tile : row) {
+            if (tile == '.' && wall_count % 2 == 1) {
+                ++interior_count;
+            }
+            if (tile == '|' || tile == 'F' || tile == '7') {
+                ++wall_count;
             }
         }
-        grid[start.row][start.col] = fill_in_start_cell(grid, start);
-
-        return { start, std::move(grid) };
+        return interior_count;
     }
-}
 
-std::optional<loc> move_cursor(const loc_set& visited, const std::vector<std::vector<cell>>& grid, const loc& current) {
-    for (const auto& next : grid[current.row][current.col]) {
-        if (!visited.contains(next)) {
-            return next;
-        }
+    int count_interior(const std::vector<std::string>& grid, const loc& start) {
+        auto loop = paint_loop(get_loop(grid, start), grid);
+        return r::fold_left(
+            loop | rv::transform(count_interior_of_row),
+            0,
+            std::plus<>()
+        );
     }
-    return {};
-}
-
-int do_part_1(const std::vector<std::vector<cell>>& grid, const loc& start) {
-    loc_set visited;
-    loc cursor_1 = grid[start.row][start.col].at(0);
-    loc cursor_2 = grid[start.row][start.col].at(1);
-    visited.insert(start);
-    visited.insert(cursor_1);
-    visited.insert(cursor_2);
-    int dist = 1;
-    while (cursor_1 != cursor_2) {
-        cursor_1 = *move_cursor(visited, grid, cursor_1);
-        cursor_2 = *move_cursor(visited, grid, cursor_2);
-        ++dist;
-        visited.insert(cursor_1);
-        visited.insert(cursor_2);
-    }
-    return dist;
-}
-
-loc_set get_loop( const std::vector<std::vector<cell>>& grid, const loc& start) {
-    loc_set visited;
-    loc cursor = start;
-
-    do {
-        visited.insert(cursor);
-        auto next = move_cursor(visited, grid, cursor);
-        cursor = (next) ? *next : start;
-    } while (cursor != start);
-
-    return visited;
-}
-
-std::vector<std::string> paint_loop(const loc_set& loop, const std::vector<std::string>& txt_grid, char start_tile) {
-    int rows = static_cast<int>(txt_grid.size());
-    int cols = static_cast<int>(txt_grid.front().size());
-    std::vector<std::string> output(rows, std::string(cols, '.'));
-    for (const auto& p : loop) {
-        auto tile = txt_grid[p.row][p.col];
-        output[p.row][p.col] = (tile != 'S') ? tile : start_tile;
-    }
-    return output;
-}
-
-enum class wall_state {
-    open_south_to_east,
-    open_north_to_east,
-    none
-};
-
-int count_interior_cells(const std::string& row) {
-    wall_state state = wall_state::none;
-    int wall_count = 0;
-    int interior_count = 0;
-
-    for (int i = 0; i < row.size(); ++i) {
-        auto tile = row[i];
-        if (tile == '.' && wall_count % 2 == 1) {
-            interior_count++;
-        } else if (tile == 'L') {
-            state = wall_state::open_north_to_east;
-        } else if( tile == 'F') {
-            state = wall_state::open_south_to_east;
-        } else if (tile == 'J') {
-            if (state == wall_state::open_south_to_east) {
-                wall_count++;
-            }
-            state = wall_state::none;
-        } else if (tile == '7') {
-            if (state == wall_state::open_north_to_east) {
-                wall_count++;
-            }
-            state = wall_state::none;
-        } else if (tile == '|') {
-            wall_count++;
-        }
-    }
-    return interior_count;
-}
-
-char get_start_tile(const std::vector<std::vector<cell>>& grid, loc start) {
-    return '|';
 }
 
 /*------------------------------------------------------------------------------------------------*/
@@ -217,12 +212,5 @@ void aoc::y2023::day_10(const std::string& title) {
 
     std::println("--- Day 10: {0} ---\n", title);
     std::println("  part 1: {}", do_part_1(grid, start));
-    auto loop = get_loop(grid, start);
-    auto loop_painting = paint_loop(loop, input, get_start_tile(grid, start));
-
-    int sum = 0;
-    for (auto row : loop_painting) {
-        sum += count_interior_cells(row);
-    }
-    std::println("  part 2: {}",sum);
+    std::println("  part 2: {}", count_interior(grid, start));
 }
