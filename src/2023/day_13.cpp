ff534a1111358f7c9ef90502b8216ebcaa45132e
Joe Wezorek <jwezorek@gmail.com>
2023-12-13 01:45:16 -0800

    day 13 part 2

--- src/2023/day_13.cpp
+++ src/2023/day_13.cpp
@@ -66,30 +66,70 @@ namespace {
         return false;
     }
 
-    std::vector<int> find_reflection_rows(const grid& g) {
-        return rv::iota(0, static_cast<int>(g.size())) | rv::filter(
+    std::optional<int> find_reflection_rows(const grid& g, std::optional<int> old = {}) {
+        auto vec = rv::iota(0, static_cast<int>(g.size())) | rv::filter(
                 [&](int row) {
                     return is_reflection_row(row, g);
                 }
             ) | r::to<std::vector<int>>();
+        if (vec.empty()) {
+            return {};
+        }
+        if (vec.size() == 1) {
+            return vec.front();
+        }
+        for (auto row : vec) {
+            if (row != *old) {
+                return row;
+            }
+        }
+        return {};
     }
 
-    std::vector<int> find_reflection_cols(const grid& g) {
+    std::optional<int> find_reflection_cols(const grid& g, std::optional<int> old = {}) {
         auto transposed = transpose(g);
-        return find_reflection_rows(transposed);
+        return find_reflection_rows(transposed, old);
+    }
+
+    std::tuple<int, int> get_scores(const grid& g, std::optional<int> old_row = {}, std::optional<int> old_col = {}) {
+        auto row = find_reflection_rows(g, old_row);
+        auto col = find_reflection_cols(g, old_col);
+
+        auto row_val = (row) ? 100 * (*row + 1) : 0;
+        auto col_val = (col) ? *col + 1 : 0;
+
+        return { col_val, row_val };
     }
 
     int summarize_pattern(const grid& g) {
-        auto rows = find_reflection_rows(g);
-        auto cols = find_reflection_cols(g);
-        return r::fold_left(
-                rows | rv::transform( [](int row)->int { return 100 * (row + 1); }), 
-                0, 
-                std::plus<>()) +
-             r::fold_left(cols | rv::transform( [](int col)->int {  return col + 1; } ), 
-                 0, 
-                 std::plus<>()
-             );
+        auto [col_val, row_val] = get_scores(g);
+        return row_val + col_val;
+    }
+
+    int summarize_pattern_with_smudge(const grid& g) {
+
+        auto old_row = find_reflection_rows(g);
+        auto old_col = find_reflection_cols(g);
+        auto [old_col_val, old_row_val] = get_scores(g);
+        auto [cols, rows] = dimensions(g);
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                auto smudge_grid = g;
+                smudge_grid[row][col] = (g[row][col] == '.') ? '#' : '.';
+                auto [new_col_val, new_row_val] = get_scores(smudge_grid, old_row, old_col);
+                if (new_col_val == 0 && new_row_val == 0) {
+                    continue;
+                }
+                if (new_col_val == old_col_val && new_row_val != old_row_val) {
+                    return new_row_val;
+                } 
+                if (new_row_val == old_row_val && new_col_val != old_col_val) {
+                    return new_col_val;
+                }
+            }
+        }
+
+        throw std::runtime_error("something is wrong");
     }
 }
 
@@ -98,12 +138,20 @@ namespace {
 void aoc::y2023::day_13(const std::string& title) {
 
     auto input = parse_input(aoc::file_to_string_vector(aoc::input_path(2023, 13)));
-    auto foo = find_reflection_rows(input.front());
+
+    //auto foo = summarize_pattern_with_smudge(input.back());
 
     std::println("--- Day 13: {0} ---\n", title);
     std::println("  part 1: {}",
         r::fold_left(
             input | rv::transform( summarize_pattern),
+            0, std::plus<>()
+        )
+    );
+    
+    std::println("  part 2: {}",
+        r::fold_left(
+            input | rv::transform(summarize_pattern_with_smudge),
             0, std::plus<>()
         )
     );
