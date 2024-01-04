aec1676beb6272dca117769baf138ab55098083a
Joe Wezorek <jwezorek@gmail.com>
2024-01-04 10:02:14 -0800

    2020, day 1 and 2

--- src/2020/day_01.cpp
+++ src/2020/day_01.cpp
@@ -4,16 +4,59 @@
 #include <functional>
 #include <print>
 #include <ranges>
-#include <unordered_map>
+#include <unordered_set>
 
 namespace r = std::ranges;
 namespace rv = std::ranges::views;
 
 /*------------------------------------------------------------------------------------------------*/
 
+namespace {
+    int do_part_1(const std::vector<int>& nums) {
+        std::unordered_set<int> num_set = nums | r::to<std::unordered_set<int>>();
+        auto num = r::find_if(nums,
+            [&num_set](int num) {
+                return num_set.contains(2020 - num);
+            }
+        );
+        return *num * (2020 - *num);
+    }
+
+    auto combos(const std::vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+        return rv::iota(0, n - 1) |
+            rv::transform(
+                [n](int i) {
+                    return rv::iota(i + 1, n) |
+                        rv::transform([i](int j) {return std::tuple<int, int>(i, j); });
+                }
+            ) | rv::join | rv::transform(
+            [&nums](auto&& tup)->std::tuple<int, int> {
+                auto [i, j] = tup;
+                return { nums.at(i), nums.at(j) };
+            }
+        );
+    }
+
+    int do_part_2(const std::vector<int>& nums) {
+        std::unordered_set<int> num_set = nums | r::to<std::unordered_set<int>>();
+        for (auto [a, b] : combos(nums)) {
+            auto c = 2020 - (a + b);
+            if (num_set.contains(c)) {
+                return a * b * c;
+            }
+        }
+        return -1;
+    }
+}
+
 void aoc::y2020::day_01(const std::string& title) {
 
+    auto nums = aoc::file_to_string_vector(aoc::input_path(2020, 1)) | rv::transform(
+        [](auto&& str) {return std::stoi(str); }
+    ) | r::to<std::vector<int>>();
+
     std::println("--- Day 1: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", do_part_1(nums) );
+    std::println("  part 2: {}", do_part_2(nums) );
 }
--- src/2020/day_02.cpp
+++ src/2020/day_02.cpp
@@ -12,12 +12,57 @@ namespace rv = std::ranges::views;
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
+
+    struct password_policy {
+        int min;
+        int max;
+        char ch;
+        std::string pwd;
+    };
+
+    password_policy parse_pwd_policy(const std::string& str) {
+        auto fields = aoc::split(str, ' ');
+        auto range = aoc::extract_numbers(fields[0]);
+        return {
+            range.front(),
+            range.back(),
+            fields[1].front(),
+            fields[2]
+        };
+    }
+
+    bool is_valid_part1(const password_policy& policy) {
+        auto count = r::count(policy.pwd, policy.ch);
+        return count >= policy.min && count <= policy.max;
+    }
+
+    bool is_valid_part2(const password_policy& policy) {
+        auto a = policy.pwd[policy.min - 1];
+        auto b = policy.pwd[policy.max - 1];
+        auto c = policy.ch;
+        return (a == c && b != c) || (b == c && a != c);
+    }
+
+    using validator = std::function<bool(const password_policy&)>;
+    int count_valid(const std::vector<password_policy>& pwds, validator validate) {
+        return r::fold_left(
+            pwds | rv::transform(
+                [validate](auto&& pwd_pol)->int {
+                    return validate(pwd_pol) ? 1 : 0;
+                }
+            ),
+            0,
+            std::plus<>()
+        );
+    }
 }
 
 void aoc::y2020::day_02(const std::string& title) {
-    auto input = aoc::file_to_string_vector(aoc::input_path(2020, 2));
+    auto input = aoc::file_to_string_vector(aoc::input_path(2020, 2)) | rv::transform(
+        parse_pwd_policy
+    ) | r::to<std::vector<password_policy>>();
 
     std::println("--- Day 2: {} ---", title);
-    std::println("  part 1: {}", 0 );
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", count_valid( input, is_valid_part1 ));
+    std::println("  part 2: {}", count_valid( input, is_valid_part2 ));
 }
