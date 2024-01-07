37bfe6d937781092811ee292e8befc237a2c0e9a
Joe Wezorek <jwezorek@gmail.com>
2024-01-07 09:08:41 -0800

    2020, days 11 and 12

--- src/2020/day_11.cpp
+++ src/2020/day_11.cpp
@@ -12,12 +12,179 @@ namespace rv = std::ranges::views;
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
+
+    using grid = std::vector<std::string>;
+
+    std::tuple<int, int> dimensions(const grid& g) {
+        return {
+            static_cast<int>(g.front().size()),
+            static_cast<int>(g.size())
+        };
+    }
+
+    struct loc {
+        int col;
+        int row;
+    };
+
+    loc operator+(const loc& lhs, const loc& rhs) {
+        return {
+            lhs.col + rhs.col,
+            lhs.row + rhs.row
+        };
+    }
+
+    bool operator==(const loc& lhs, const loc& rhs) {
+        return lhs.col == rhs.col && lhs.row == rhs.row;
+    }
+
+    auto direction_deltas() {
+        static const std::array<loc, 8> deltas = {
+            loc{-1,-1}, loc{0,-1}, loc{1,-1},
+            loc{-1, 0},            loc{1, 0},
+            loc{-1, 1}, loc{0, 1}, loc{1, 1},
+        };
+        return rv::all(deltas);
+    }
+
+    bool in_bounds(const loc& p, int cols, int rows) {
+        return p.col >= 0 && p.col < cols &&
+            p.row >= 0 && p.row < rows;
+    }
+
+    auto neighbors(const loc& pos, int cols, int rows) {
+        return direction_deltas() | rv::transform(
+            [pos](auto&& delta)->loc {
+                return pos + delta;
+            }
+        ) | rv::filter(
+            [cols,rows](auto&& p)->bool {
+                return in_bounds(p, cols, rows);
+            }
+        );
+    }
+
+    auto all_locs(int cols, int rows) {
+        return rv::cartesian_product(
+                rv::iota(0, cols), rv::iota(0, rows)
+            ) | rv::transform(
+                [](auto&& tup)->loc {
+                    auto [col, row] = tup;
+                    return loc{ col,row };
+                }
+            );
+    }
+
+    grid make_grid(int cols, int rows) {
+        return grid(rows, std::string(cols, '.'));
+    }
+
+    using occupied_fn = std::function<int(const grid&, const loc&)>;
+
+    int immediate_occupied_neigbors(const grid& g, const loc& where) {
+        auto [cols, rows] = dimensions(g);
+        return r::fold_left(
+            neighbors(where, cols, rows) | rv::transform(
+                [&g](auto&& pt)->int {
+                    return g[pt.row][pt.col] == '#' ? 1 : 0;
+                }
+            ),
+            0,
+            std::plus<>()
+        );
+    }
+
+    char line_of_sight(const grid& g, const loc& pos, const loc& delta) {
+        auto [cols, rows] = dimensions(g);
+        loc los_pt = pos + delta;
+        while (in_bounds(los_pt, cols, rows) && g[los_pt.row][los_pt.col] == '.') {
+            los_pt = los_pt + delta;
+        }
+        return in_bounds(los_pt, cols, rows) ? g[los_pt.row][los_pt.col] : '.';
+    }
+
+    int occupied_neigbors_line_of_sight(const grid& g, const loc& where) {
+        return r::fold_left(
+            direction_deltas() | rv::transform(
+                [&](auto&& delta)->int {
+                    return line_of_sight(g, where, delta) == '#';
+                }
+            ),
+            0,
+            std::plus<>()
+        );
+    }
+
+    std::tuple<grid, bool> do_one_iteration(const grid& g, occupied_fn neighbor_fn, int occupied_threshold) {
+        auto [cols, rows] = dimensions(g);
+        auto next = make_grid(cols, rows);
+        bool change_occurred = false;
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                auto tile = g[row][col];
+                if (tile == '.') {
+                    continue;
+                }
+                int occupied_neighbors = neighbor_fn(g, { col,row });
+                char new_tile = tile;
+                if (tile == 'L' && occupied_neighbors == 0) {
+                    new_tile = '#';
+                    change_occurred = true;
+                } else if (tile == '#' && occupied_neighbors >= occupied_threshold){
+                    new_tile = 'L';
+                    change_occurred = true;
+                }
+                next[row][col] = new_tile;
+            }
+        }
+        return { std::move(next), change_occurred };
+    }
+
+    int count_occupied(const grid& g) {
+        auto [cols, rows] = dimensions(g);
+        return r::fold_left(
+            all_locs(cols, rows) | rv::transform(
+                [&g](auto&& pos)->int {
+                    return g[pos.row][pos.col] == '#' ? 1 : 0;
+                }
+            ),
+            0,
+            std::plus<>()
+        );
+    }
+
+    void display(const grid& g) {
+        for (const auto& row : g) {
+            std::println("{}", row);
+        }
+        std::println("");
+    }
+
+    int count_occupied_at_equilibrium(const grid& g, 
+            occupied_fn neighbor_fn, int occupied_threshold) {
+        auto grid = g;
+        bool change_occurred = false;
+        do {
+            std::tie(grid, change_occurred) = do_one_iteration(
+                grid, 
+                neighbor_fn,
+                occupied_threshold
+            );
+        } while (change_occurred);
+
+        return count_occupied(grid);
+    }
 }
 
 void aoc::y2020::day_11(const std::string& title) {
     auto input = aoc::file_to_string_vector(aoc::input_path(2020, 11));
 
     std::println("--- Day 11: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
+    
+    std::println("  part 1: {}", 
+        count_occupied_at_equilibrium(input, immediate_occupied_neigbors, 4) 
+    );
+    std::println("  part 2: {}",
+        count_occupied_at_equilibrium(input, occupied_neigbors_line_of_sight, 5)
+    );
 }
--- src/2020/day_12.cpp
+++ src/2020/day_12.cpp
@@ -12,12 +12,152 @@ namespace rv = std::ranges::views;
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
+    struct vec2 {
+        int x;
+        int y;
+    };
+
+    vec2 operator+(const vec2& lhs, const vec2& rhs) {
+        return {
+            lhs.x + rhs.x,
+            lhs.y + rhs.y
+        };
+    }
+
+    vec2 operator*(int lhs, const vec2& rhs) {
+        return {
+            lhs * rhs.x,
+            lhs * rhs.y
+        };
+    }
+
+    bool operator==(const vec2& lhs, const vec2 rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    enum direction {
+        north = 0, east, south, west
+    };
+
+    enum class instr_dir {
+        north = 0, east, south, west, left, right, forward
+    };
+
+    vec2 direction_delta(direction dir) {
+        static const std::array<vec2, 4> deltas = {
+            vec2{0,1},vec2{1,0},vec2{0,-1},vec2{-1,0}
+        };
+        return deltas.at(dir);
+    }
+
+    int wrap(int n, int max) {
+        return ((n % max) + max) % max;
+    }
+
+    direction change_direction(direction dir, bool right, int degrees) {
+        int index = degrees / 90;
+        if (!right) {
+            index *= -1;
+        }
+        return static_cast<direction>(wrap(dir + index, 4));
+    }
+
+    struct pos_dir {
+        vec2 pos;
+        direction dir;
+    };
+
+    struct nav_instruction {
+        instr_dir dir;
+        int amnt;
+    };
+
+    struct ship_and_wp {
+        vec2 ship;
+        vec2 waypoint;
+    };
+
+    nav_instruction parse_nav_instruction(const std::string& str) {
+        static const std::unordered_map<char, instr_dir> char_to_dir = {
+            {'N', instr_dir::north},
+            {'E', instr_dir::east},
+            {'S', instr_dir::south},
+            {'W', instr_dir::west},
+            {'L', instr_dir::left},
+            {'R', instr_dir::right},
+            {'F', instr_dir::forward}
+        };
+        return {
+            char_to_dir.at(str.front()),
+            std::stoi(str.substr(1, str.size() - 1))
+        };
+    }
+
+    pos_dir do_nav_instruction(const pos_dir& pd, const nav_instruction& instr) {
+        if (instr.dir >= instr_dir::north && instr.dir <= instr_dir::west) {
+            return { pd.pos + instr.amnt * direction_delta(static_cast<direction>(instr.dir)), pd.dir };
+        } 
+        if (instr.dir == instr_dir::forward) {
+            return { pd.pos + instr.amnt * direction_delta(pd.dir), pd.dir };
+        }
+        return { pd.pos, change_direction(pd.dir, instr.dir == instr_dir::right, instr.amnt) };
+    }
+
+    int follow_nav_instructions(const std::vector<nav_instruction>& instructions) {
+        pos_dir ship = { {0,0}, east };
+        for (const auto& instruction : instructions) {
+            ship = do_nav_instruction(ship, instruction);
+        }
+        return std::abs(ship.pos.x) + std::abs(ship.pos.y);
+    }
+
+    vec2 rotate(vec2 pos, bool right, int amnt) {
+        amnt /= 90;
+        auto rotate_right = [](const vec2& p)->vec2 {
+            return { p.y, -p.x };
+        };
+        auto rotate_left = [](const vec2& p)->vec2 {
+            return { -p.y, p.x };
+            };
+        for (int i = 0; i < amnt; ++i) {
+            pos = (right) ? rotate_right(pos) : rotate_left(pos);
+        }
+        return pos;
+    }
+
+    ship_and_wp do_nav_instruction(const ship_and_wp& sw, const nav_instruction& instr) {
+        if (instr.dir >= instr_dir::north && instr.dir <= instr_dir::west) {
+            return {
+                sw.ship,
+                sw.waypoint + instr.amnt * direction_delta(static_cast<direction>(instr.dir))
+            };
+        }
+        if (instr.dir == instr_dir::forward) {
+            return {
+                sw.ship + instr.amnt * sw.waypoint,
+                sw.waypoint
+            };
+        }
+        return {
+            sw.ship,
+            rotate(sw.waypoint, instr.dir == instr_dir::right, instr.amnt)
+        };
+    }
+
+    int follow_waypoint_nav_instructions(const std::vector<nav_instruction>& instructions) {
+        ship_and_wp state{ {0,0},{10,1} };
+        for (const auto& instruction : instructions) {
+            state = do_nav_instruction(state, instruction);
+        }
+        return std::abs(state.ship.x) + std::abs(state.ship.y);
+    }
 }
 
 void aoc::y2020::day_12(const std::string& title) {
-    auto input = aoc::file_to_string_vector(aoc::input_path(2020, 12));
+    auto input = aoc::file_to_string_vector(aoc::input_path(2020, 12)) |
+        rv::transform(parse_nav_instruction) | r::to<std::vector<nav_instruction>>();
 
     std::println("--- Day 12: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", follow_nav_instructions(input));
+    std::println("  part 2: {}", follow_waypoint_nav_instructions(input));
 }
