d5d536bab9ad02bcb0fb15fb3983c0487928fbb4
Joe Wezorek <jwezorek@gmail.com>
2024-01-09 11:17:02 -0800

    working on 2020 day 19 part 2

--- src/2020/day_19.cpp
+++ src/2020/day_19.cpp
@@ -5,6 +5,7 @@
 #include <print>
 #include <ranges>
 #include <unordered_map>
+#include <unordered_set>
 #include <memory>
 #include <optional>
 
@@ -19,14 +20,10 @@ namespace {
     using rule_tbl = std::unordered_map<int, std::shared_ptr<rule>>;
 
     class rule {
-    protected:
-        std::vector<int> child_rules_;
     public:
-        rule(const std::vector<int>& child_rules) : child_rules_(child_rules) {
-        }
-        virtual void populate(const rule_tbl& rules) = 0;
-        virtual int check() const = 0;
-        virtual std::optional<int> validate(const std::string& inp, int pos) const = 0;
+        virtual int check(const rule_tbl& rules) const = 0;
+        virtual std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const = 0;
+        virtual std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const = 0;
     };
 
     using rule_ptr = std::shared_ptr<rule>;
@@ -34,17 +31,14 @@ namespace {
     class literal_rule : public rule {
         char letter_;
     public:
-        literal_rule(char letter) : rule(std::vector<int>{}), letter_(letter) {
+        literal_rule(char letter) : letter_(letter) {
         }
 
-        void populate(const rule_tbl& rules) override {
-        }
-
-        int check() const override {
+        int check(const rule_tbl& rules) const override {
             return 1;
         }
 
-        std::optional<int> validate(const std::string& inp, int pos) const override {
+        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
             if (pos >= inp.size()) {
                 return {};
             }
@@ -53,35 +47,31 @@ namespace {
             }
             return pos + 1;
         }
+
+        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+            auto result = validate(rules, inp, pos);
+            if (result) {
+                return { *result };
+            } else {
+                return {};
+            }
+        }
     };
 
 
     class conjunction_rule : public rule {
-        std::vector<rule_ptr> children_;
+        std::vector<int> children_;
     public:
         conjunction_rule(const std::vector<int>& child_rules) :
-            rule(child_rules) {
+            children_(child_rules) {
         }
 
-        conjunction_rule(rule_ptr first, rule_ptr second) :
-            rule({}) {
-            children_ = { first, second };
-        }
-
-        void populate(const rule_tbl& rules) override {
-            children_ = child_rules_ | rv::transform(
-                [&rules](int index) {
-                    return rules.at(index);
-                }
-            ) | r::to<std::vector<rule_ptr>>();
-        }
-
-        std::optional<int> validate(const std::string& inp, int pos) const override {
+        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
             if (pos >= inp.size()) {
                 return {};
             }
             for (auto child : children_) {
-                auto new_pos = child->validate(inp, pos);
+                auto new_pos = rules.at(child)->validate(rules, inp, pos);
                 if (!new_pos) {
                     return {};
                 }
@@ -90,62 +80,67 @@ namespace {
             return pos;
         }
 
-        int check() const override {
+        int check(const rule_tbl& rules) const override {
             auto checks = children_ | rv::transform(
-                [](auto child) {return child->check(); }
+                [&rules](auto child) {return rules.at(child)->check(rules); }
             );
             if (r::find(checks, -1) != checks.end()) {
                 return -1;
             }
             return r::fold_left(checks, 0, std::plus<>());
         }
+
+        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+            /*
+            if (pos >= inp.size()) {
+                return {};
+            }
+            std::unordered_set<int> results;
+
+            for (auto child : children_) {
+                auto new_positions = rules.at(child)->full_validate(rules, inp, pos);
+                if (!new_pos) {
+                    return {};
+                }
+                pos = *new_pos;
+            }
+            */
+            return {};
+        }
     };
 
     class disjunction_rule : public rule {
         std::vector<rule_ptr> children_;
     public:
-        disjunction_rule(const std::vector<int>& child_rules) :
-            rule(child_rules) {
+        disjunction_rule(const std::vector<int>& lhs, const std::vector<int>& rhs) {
+            children_.push_back(std::make_shared<conjunction_rule>(lhs));
+            children_.push_back(std::make_shared<conjunction_rule>(rhs));
         }
 
-        void populate(const rule_tbl& rules) override {
-            auto subrules = child_rules_ | rv::transform(
-                [&rules](int index) {
-                    return rules.at(index);
-                }
-            ) | r::to<std::vector<rule_ptr>>();
-            rule_ptr lhs, rhs;
-            if (subrules.size() == 4) {
-                lhs = std::make_shared<conjunction_rule>(subrules[0], subrules[1]);
-                rhs = std::make_shared<conjunction_rule>(subrules[2], subrules[3]);
-            } else if (subrules.size() == 2) {
-                lhs = subrules[0];
-                rhs = subrules[1];
-            } else {
-                throw std::runtime_error("invalid disjunction rule");
-            }
-            children_ = { lhs, rhs };
-        }
-
-        int check() const override {
-            auto a = children_.front()->check();
-            auto b = children_.back()->check();
+        int check(const rule_tbl& rules) const override {
+            auto a = children_.front()->check(rules);
+            auto b = children_.back()->check(rules);
             if (a == -1 || b == -1) {
                 return -1;
             }
             return (a == b) ? a : -1;
         }
 
-        std::optional<int> validate(const std::string& inp, int pos) const override {
+        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
             if (pos >= inp.size()) {
                 return {};
             }
             for (auto child : children_) {
-                auto new_pos = child->validate(inp, pos);
+                auto new_pos = child->validate(rules, inp, pos);
                 if (new_pos) {
                     return new_pos;
                 }
             }
+            return {};
+        }
+
+        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+            std::unordered_set<int> results;
             return {};
         }
     };
@@ -161,8 +156,11 @@ namespace {
     }
 
     rule_ptr parse_disjunction(const std::string& str) {
-        auto nums = aoc::extract_numbers(str);
-        return std::make_shared<disjunction_rule>(nums);
+        auto [lhs,rhs] = aoc::split_to_tuple<2>(str, '|');
+        return std::make_shared<disjunction_rule>(
+            aoc::extract_numbers(lhs), 
+            aoc::extract_numbers(rhs)
+       );
     }
 
     rule_tbl::value_type parse_rule(const std::string& str) {
@@ -182,11 +180,7 @@ namespace {
     }
 
     rule_tbl parse_rules(const std::vector<std::string>& input) {
-        auto rules = input | rv::transform(parse_rule) | r::to<rule_tbl>();
-        for (auto rule : rules | rv::values) {
-            rule->populate(rules);
-        }
-        return rules;
+        return input | rv::transform(parse_rule) | r::to<rule_tbl>();
     }
 
     std::tuple<rule_tbl, std::vector<std::string>> parse_input(
@@ -198,8 +192,8 @@ namespace {
         };
     }
 
-    bool is_match(rule_ptr rule, const std::string& inp) {
-        auto result = rule->validate(inp, 0);
+    bool is_match(const rule_tbl& rules, rule_ptr rule, const std::string& inp) {
+        auto result = rule->validate(rules, inp, 0);
         if (!result) {
             return false;
         }
@@ -210,8 +204,45 @@ namespace {
         return true;
     }
 
-    int do_part_2(const rule_tbl& rules, const std::vector<std::string>& mesages) {
-        return 0;
+    int do_part_2(const rule_tbl& rules, const std::vector<std::string>& messages) {
+        auto new_rules = rules;
+        new_rules[8] = std::get<1>(parse_rule("8: 42 | 42 8"));
+        new_rules[11] = std::get<1>(parse_rule("11: 42 31 | 42 11 31"));
+
+        return r::fold_left(
+            messages | rv::transform(
+                [&new_rules](auto&& str)->int {
+                    return is_match(new_rules, new_rules.at(0), str) ? 1 : 0;
+                }
+            ),
+            0,
+            std::plus<>()
+        );
+    }
+
+    void test(const rule_tbl& rules) {
+        auto new_rules = rules;
+        new_rules[8] = std::get<1>(parse_rule("8: 42 | 42 8"));
+        new_rules[11] = std::get<1>(parse_rule("11: 42 31 | 42 11 31"));
+
+        std::vector<std::string> test_msgs = {
+            //"bbabbbbaabaabba",
+            "babbbbaabbbbbabbbbbbaabaaabaaa",
+            "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+            "bbbbbbbaaaabbbbaaabbabaaa",
+            "bbbababbbbaaaaaaaabbababaaababaabab",
+            "ababaaaaaabaaab",
+            "ababaaaaabbbaba",
+            "baabbaaaabbaaaababbaababb",
+            "abbbbabbbbaaaababbbbbbaaaababb",
+            "aaaaabbaabaaaaababaa",
+            "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+            "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+        };
+
+        for (auto msg : test_msgs) {
+            std::println("{} => {}", msg, is_match(new_rules, new_rules[0], msg));
+        }
     }
 }
 
@@ -220,13 +251,13 @@ void aoc::y2020::day_19(const std::string& title) {
     auto [rules, messages] = parse_input(
         aoc::file_to_string_vector(aoc::input_path(2020, 19))
     );
-
+    
     std::println("--- Day 19: {} ---", title);
     std::println("  part 1: {}", 
         r::fold_left(
             messages | rv::transform(
                 [&rules](auto&& str)->int {
-                    return is_match(rules.at(0), str) ? 1 : 0;
+                    return is_match(rules, rules.at(0), str) ? 1 : 0;
                 }
             ),
             0,
@@ -234,5 +265,7 @@ void aoc::y2020::day_19(const std::string& title) {
         )
     );
     std::println("  part 2: {}", do_part_2(rules, messages));
+    
+    //test(rules);
     
 }
