fe37b1540b1ba62b1865d699c3936bbcb81bec58
Joe Wezorek <jwezorek@gmail.com>
2024-01-09 11:56:08 -0800

    2020 day 19

--- src/2020/day_19.cpp
+++ src/2020/day_19.cpp
@@ -22,9 +22,7 @@ namespace {
 
     class rule {
     public:
-        virtual int check(const rule_tbl& rules) const = 0;
-        virtual std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const = 0;
-        virtual std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const = 0;
+        virtual std::vector<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const = 0;
     };
 
     using rule_ptr = std::shared_ptr<rule>;
@@ -35,30 +33,16 @@ namespace {
         literal_rule(char letter) : letter_(letter) {
         }
 
-        int check(const rule_tbl& rules) const override {
-            return 1;
-        }
-
-        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+        std::vector<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
             if (pos >= inp.size()) {
                 return {};
             }
             if (inp[pos] != letter_) {
                 return {};
             }
-            return pos + 1;
-        }
-
-        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
-            auto result = validate(rules, inp, pos);
-            if (result) {
-                return { *result };
-            } else {
-                return {};
-            }
+            return std::vector<int>{ pos + 1 };
         }
     };
-
 
     class conjunction_rule : public rule {
         std::vector<int> children_;
@@ -67,39 +51,16 @@ namespace {
             children_(child_rules) {
         }
 
-        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+        std::vector<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+            struct state {
+                int child;
+                int pos;
+            };
+
             if (pos >= inp.size()) {
                 return {};
             }
-            for (auto child : children_) {
-                auto new_pos = rules.at(child)->validate(rules, inp, pos);
-                if (!new_pos) {
-                    return {};
-                }
-                pos = *new_pos;
-            }
-            return pos;
-        }
 
-        int check(const rule_tbl& rules) const override {
-            auto checks = children_ | rv::transform(
-                [&rules](auto child) {return rules.at(child)->check(rules); }
-            );
-            if (r::find(checks, -1) != checks.end()) {
-                return -1;
-            }
-            return r::fold_left(checks, 0, std::plus<>());
-        }
-
-        struct state {
-            int child;
-            int pos;
-        };
-
-        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
-            if (pos >= inp.size()) {
-                return {};
-            }
             std::unordered_set<int> results;
             std::stack<state> stack;
             stack.push({ 0,pos });
@@ -107,7 +68,7 @@ namespace {
                 state s = stack.top();
                 stack.pop();
                 if (s.child < children_.size()) {
-                    auto new_positions = rules.at(children_[s.child])->full_validate(rules, inp, s.pos);
+                    auto new_positions = rules.at(children_[s.child])->validate(rules, inp, s.pos);
                     for (int new_pos : new_positions) {
                         stack.push({ s.child + 1, new_pos });
                     }
@@ -115,6 +76,7 @@ namespace {
                     results.insert(s.pos);
                 }
             }
+
             return results | r::to<std::vector<int>>();
         }
     };
@@ -127,35 +89,13 @@ namespace {
             children_.push_back(std::make_shared<conjunction_rule>(rhs));
         }
 
-        int check(const rule_tbl& rules) const override {
-            auto a = children_.front()->check(rules);
-            auto b = children_.back()->check(rules);
-            if (a == -1 || b == -1) {
-                return -1;
-            }
-            return (a == b) ? a : -1;
-        }
-
-        std::optional<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
-            if (pos >= inp.size()) {
-                return {};
-            }
-            for (auto child : children_) {
-                auto new_pos = child->validate(rules, inp, pos);
-                if (new_pos) {
-                    return new_pos;
-                }
-            }
-            return {};
-        }
-
-        std::vector<int> full_validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
+        std::vector<int> validate(const rule_tbl& rules, const std::string& inp, int pos) const override {
             if (pos >= inp.size()) {
                 return {};
             }
             std::unordered_set<int> results;
             for (auto child : children_) {
-                auto new_positions = child->full_validate(rules, inp, pos);
+                auto new_positions = child->validate(rules, inp, pos);
                 if (!new_positions.empty()) {
                     for (int new_pos : new_positions) {
                         results.insert(new_pos);
@@ -215,18 +155,6 @@ namespace {
 
     bool is_match(const rule_tbl& rules, rule_ptr rule, const std::string& inp) {
         auto result = rule->validate(rules, inp, 0);
-        if (!result) {
-            return false;
-        }
-        if (*result != inp.size()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    bool is_full_match(const rule_tbl& rules, rule_ptr rule, const std::string& inp) {
-        auto result = rule->full_validate(rules, inp, 0);
         if (result.empty()) {
             return false;
         }
@@ -237,19 +165,19 @@ namespace {
         return true;
     }
 
-    int do_part_2(const rule_tbl& rules, const std::vector<std::string>& messages) {
+    rule_tbl new_rules(const rule_tbl& rules) {
         auto new_rules = rules;
         new_rules[8] = std::get<1>(parse_rule("8: 42 | 42 8"));
         new_rules[11] = std::get<1>(parse_rule("11: 42 31 | 42 11 31"));
 
-        return r::fold_left(
-            messages | rv::transform(
-                [&new_rules](auto&& str)->int {
-                    return is_full_match(new_rules, new_rules.at(0), str) ? 1 : 0;
-                }
-            ),
-            0,
-            std::plus<>()
+        return new_rules;
+    }
+
+    int count_valid_messages(const rule_tbl& rules, const std::vector<std::string>& messages) {
+        return r::count_if(messages,
+            [&rules](auto&& str)->int {
+                return is_match(rules, rules.at(0), str);
+            }
         );
     }
 }
@@ -261,17 +189,7 @@ void aoc::y2020::day_19(const std::string& title) {
     );
     
     std::println("--- Day 19: {} ---", title);
-    std::println("  part 1: {}", 
-        r::fold_left(
-            messages | rv::transform(
-                [&rules](auto&& str)->int {
-                    return is_match(rules, rules.at(0), str) ? 1 : 0;
-                }
-            ),
-            0,
-            std::plus<>()
-        )
-    );
-    std::println("  part 2: {}", do_part_2(rules, messages));
+    std::println("  part 1: {}", count_valid_messages( rules, messages ));
+    std::println("  part 2: {}", count_valid_messages( new_rules(rules), messages ));
     
 }
