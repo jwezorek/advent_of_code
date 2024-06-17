15aec1943a026653cb99e870d00241c1c4297994
joe wezorek <jwezorek@gmail.com>
2024-06-17 15:43:19 -0700

    2019 day 12 ... redid part1 to do all calucaltions/simulation per axis and then do part2 by taking the lcm of cycles on each axis

--- src/2019/day_12.cpp
+++ src/2019/day_12.cpp
@@ -5,6 +5,7 @@
 #include <print>
 #include <ranges>
 #include <unordered_set>
+#include <numeric>
 #include <boost/functional/hash.hpp>
 
 namespace r = std::ranges;
@@ -14,112 +15,156 @@ namespace rv = std::ranges::views;
 
 namespace {
 
-    struct vec3 {
-        int x;
-        int y;
-        int z;
+    struct axis_state {
+        int position;
+        int velocity;
 
-        bool operator==(const vec3& p) const {
-            return x == p.x && y == p.y && z == p.z;
+        axis_state(int p = 0, int v = 0) : position{ p }, velocity{ v } {
         }
     };
 
-    vec3 operator+(const vec3& lhs, const vec3& rhs) {
-        return  { lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z };
-    }
+    constexpr int k_x_axis = 0;
+    constexpr int k_y_axis = 1;
+    constexpr int k_z_axis = 2;
 
-    struct vec3_hash {
-        size_t operator()(const vec3& pt) const {
+    using system_axis_state = std::array<axis_state, 4>;
+    using system_state = std::array<system_axis_state, 3>;
+
+    struct system_axis_state_hash {
+        size_t operator()(const system_axis_state& sys_axis) const {
             size_t seed = 0;
-            boost::hash_combine(seed, pt.x);
-            boost::hash_combine(seed, pt.y);
-            boost::hash_combine(seed, pt.z);
+            for (const auto& moon_axis : sys_axis) {
+                boost::hash_combine( seed, moon_axis.position );
+                boost::hash_combine( seed, moon_axis.velocity );
+            }
             return seed;
         }
     };
 
-    struct moon {
-        vec3 position;
-        vec3 velocity;
+    bool operator==(const system_axis_state& lhs, const system_axis_state& rhs) {
+        for (const auto& [a,b] : rv::zip(lhs,rhs)) {
+            if (a.position != b.position || a.velocity != b.velocity) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        moon(const vec3 p) :
-            position{ p },
-            velocity{ 0, 0, 0 }
-        {}
-    };
-
-    std::vector<moon> parse_input(const std::vector<std::string>& inp) {
-        return inp | rv::transform(
-            [](auto&& line)->moon {
+    system_state parse_system_state(const std::vector<std::string>& inp) {
+        auto moon_positions = inp | rv::transform(
+            [](auto&& line)->std::tuple<int,int,int> {
                 auto vals = aoc::extract_numbers(line, true);
-                return moon({ vals[0], vals[1], vals[2] });
+                return { vals[0], vals[1], vals[2] };
             }
         ) | r::to<std::vector>();
-    }
 
-    void update_vel_comp(moon& m1, moon& m2, std::function<int&(vec3&)> get) {
+        system_state sys = {{
+            {{ {}, {}, {}, {} }},
+            {{ {}, {}, {}, {} }},
+            {{ {}, {}, {}, {} }}
+        }};
 
-        int v1 = get(m1.velocity);
-        int v2 = get(m2.velocity);
-        int pos1 = get(m1.position);
-        int pos2 = get(m2.position);
-
-        if (pos1 == pos2) {
-            return;
+        for (const auto& [moon_index, position] : rv::enumerate(moon_positions)) {
+            auto [x, y, z] = position;
+            sys[k_x_axis][moon_index].position = x;
+            sys[k_y_axis][moon_index].position = y;
+            sys[k_z_axis][moon_index].position = z;
         }
 
-        auto delta = (pos2 > pos1) ? 1 : -1;
-        get(m1.velocity) = v1 + delta;
-        get(m2.velocity) = v2 + (-1 * delta);
+        return sys;
     }
 
-    void update_velocities(std::vector<moon>& moons) {
-        for (auto [i, j] : aoc::two_combos_indices(moons.size())) {
-            update_vel_comp( moons[i], moons[j], [](vec3& v)->int& { return v.x; } );    
-            update_vel_comp( moons[i], moons[j], [](vec3& v)->int& { return v.y; } );   
-            update_vel_comp( moons[i], moons[j], [](vec3& v)->int& { return v.z; } );
+    int vel_delta_on_axis(const axis_state& lhs, const axis_state& rhs) {
+        if (lhs.position == rhs.position) {
+            return 0;
+        }
+        return (rhs.position > lhs.position) ? 1 : -1;
+    }
+
+    void update_vel_on_axis(system_axis_state& state) {
+        for (auto [i, j] : aoc::two_combos_indices(state.size())) {
+            auto i_to_j_delta = vel_delta_on_axis(state[i], state[j]);
+            state[i].velocity += i_to_j_delta;
+            state[j].velocity -= i_to_j_delta;
         }
     }
 
-    void simulate_moons(std::vector<moon>& moons, int steps) {
-        for (int i = 0; i < steps; ++i) {
-            update_velocities(moons);
-            for (auto& moon : moons) {
-                moon.position = moon.position + moon.velocity;
+    void simulate_n_timesteps_on_axis(system_axis_state& sys_axis_state, int n) {
+        for (int i = 0; i < n; ++i) {
+            update_vel_on_axis(sys_axis_state);
+            for (auto& moon_axis_state : sys_axis_state) {
+                moon_axis_state.position += moon_axis_state.velocity;
             }
         }
     }
 
-    int energy_of_vec3(const vec3& v) {
-        return std::abs(v.x) + std::abs(v.y) + std::abs(v.z);
+    std::array<int, 6> moon_state(system_state& sys_state, int moon_index) {
+        return {
+            sys_state[k_x_axis][moon_index].position,
+            sys_state[k_y_axis][moon_index].position,
+            sys_state[k_z_axis][moon_index].position,
+
+            sys_state[k_x_axis][moon_index].velocity,
+            sys_state[k_y_axis][moon_index].velocity,
+            sys_state[k_z_axis][moon_index].velocity
+        };
     }
 
-    int energy_after_n_steps(const std::vector<moon>& inp, int n) {
-        auto moons = inp;
-        simulate_moons(moons, n);
+    int energy_after_n_timesteps(system_state& sys_state, int n) {
+        auto sys = sys_state;
+
+        simulate_n_timesteps_on_axis(sys[k_x_axis], n);
+        simulate_n_timesteps_on_axis(sys[k_y_axis], n);
+        simulate_n_timesteps_on_axis(sys[k_z_axis], n);
+
         return r::fold_left(
-            moons | rv::transform(
-                [](auto&& m)->int {
-                    return energy_of_vec3(m.position) * energy_of_vec3(m.velocity);
+            rv::iota(0,4) | rv::transform(
+                [&](auto moon_index)->int {
+                    auto [x, y, z, x_vel, y_vel, z_vel] = moon_state(sys, moon_index);
+                    return (std::abs(x) + std::abs(y) + std::abs(z)) *
+                        (std::abs(x_vel) + std::abs(y_vel) + std::abs(z_vel));
                 }
             ),
             0,
             std::plus<>()
         );
     }
+
+    // this function assumes that the intial state of each axis eventually repeats,
+    // and is the first state to repeat...
+
+    int64_t find_cycle_on_axis(const system_axis_state& initial_state) {
+        int64_t steps = 0;
+        auto state = initial_state;
+
+        do {
+            simulate_n_timesteps_on_axis(state, 1);
+            ++steps;
+        } while (state != initial_state);
+
+        return steps;
+    }
  }
 
 void aoc::y2019::day_12(const std::string& title) {
 
-    auto moons = parse_input(
-        aoc::file_to_string_vector(aoc::input_path(2019, 12))
-    );
+    auto initial_sys_state = parse_system_state(
+            aoc::file_to_string_vector(aoc::input_path(2019, 12))
+        );
 
     std::println("--- Day 12: {} ---", title);
+
     std::println("  part 1: {}",
-        energy_after_n_steps(moons, 1000)
+        energy_after_n_timesteps(initial_sys_state, 1000)
     );
+
     std::println("  part 2: {}",
-        0
+        std::lcm( 
+            find_cycle_on_axis(initial_sys_state[0]),
+            std::lcm(
+                find_cycle_on_axis(initial_sys_state[1]),
+                find_cycle_on_axis(initial_sys_state[2])
+            )
+        )
     );
 }
