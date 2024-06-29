165017940180689fa53d8732e39f03b2a3b50b4d
joe wezorek <jwezorek@gmail.com>
2024-06-29 14:56:08 -0700

    change 2015 day 12 to just use boost.json so i dont have to include another dependency

--- src/2015/day_12.cpp
+++ src/2015/day_12.cpp
@@ -5,38 +5,55 @@
 #include <print>
 #include <ranges>
 #include <unordered_set>
-#include "../json.hpp"
+#define BOOST_JSON_NO_LIB 
+#define BOOST_CONTAINER_NO_LIB
+#include <boost/json/src.hpp>
 
 namespace r = std::ranges;
 namespace rv = std::ranges::views;
 
-using json = nlohmann::json;
-
 /*------------------------------------------------------------------------------------------------*/
+
+using json = boost::json::value;
 
 namespace {
 
     int recursive_sum_of_numbers_in_non_red_objects(const json& json) {
 
         if (json.is_number()) {
-            auto num = json.get<int>();
-            return num;
+            auto num = json.as_int64();
+            return static_cast<int>(num);
         }
 
-        if (json.is_structured()) {
-            int sum = 0;
-            if (json.is_object()) {
-                for (auto item : json.items()) {
-                    const auto& value = item.value();
-                    if (value.is_string() && value.get<std::string>() == "red") {
-                        return 0;
+        if (json.is_array()) {
+            const auto& ary = json.as_array();
+            return r::fold_left(
+                ary | rv::transform(recursive_sum_of_numbers_in_non_red_objects),
+                0,
+                std::plus<>()
+            );
+        }
+
+        if (json.is_object()) {
+            const auto& obj = json.as_object();
+            int red_properties = r::count_if(
+                    obj,
+                    [](auto&& kvp) {
+                        return kvp.value().is_string() && kvp.value().as_string() == "red";
                     }
-                }
+                );
+            if (red_properties > 0) {
+                return 0;
             }
-            for (auto item : json.items()) {
-                sum += recursive_sum_of_numbers_in_non_red_objects(item.value());
-            }
-            return sum;
+            return r::fold_left(
+                obj | rv::transform(
+                    [](auto&& kvp) {
+                        return recursive_sum_of_numbers_in_non_red_objects(kvp.value());
+                    }
+                ),
+                0,
+                std::plus<>()
+            );
         }
 
         return 0;
@@ -59,7 +76,7 @@ void aoc::y2015::day_12(const std::string& title) {
     
     std::println("  part 2: {}",
         recursive_sum_of_numbers_in_non_red_objects(
-            json::parse(inp)
+            boost::json::parse(inp)
         )
     );
 }
