c2a3642a6a6c57d5319a4d2f626c057322b9d80e
joe wezorek <jwezorek@gmail.com>
2024-06-29 09:52:42 -0700

    2015 day 12

--- src/2015/day_12.cpp
+++ src/2015/day_12.cpp
@@ -5,25 +5,60 @@
 #include <print>
 #include <ranges>
 #include <unordered_set>
+#include "../json.hpp"
 
 namespace r = std::ranges;
 namespace rv = std::ranges::views;
+
+using json = nlohmann::json;
 
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
 
+    int recursive_sum_of_numbers_of_non_red_objects(const json& json) {
+
+        if (json.is_number()) {
+            auto num = json.get<int>();
+            return num;
+        }
+
+        if (json.is_array() || json.is_object()) {
+            int sum = 0;
+            if (json.is_object()) {
+                for (auto item : json.items()) {
+                    if (item.value().is_string() && item.value().get<std::string>() == "red") {
+                        return 0;
+                    }
+                }
+            }
+            for (auto item : json.items()) {
+                sum += recursive_sum_of_numbers_of_non_red_objects(item.value());
+            }
+            return sum;
+        }
+
+        return 0;
+    }
+
 }
 
 void aoc::y2015::day_12(const std::string& title) {
 
-    auto nums = aoc::file_to_string_vector(aoc::input_path(2015, 12));
+    auto inp = aoc::file_to_string(aoc::input_path(2015, 12));
 
     std::println("--- Day 12: {} ---", title);
     std::println("  part 1: {}",
-        0
+        r::fold_left(
+            aoc::extract_numbers(inp, true),
+            0,
+            std::plus<>()
+        )
     );
+    
     std::println("  part 2: {}",
-        0
+        recursive_sum_of_numbers_of_non_red_objects(
+            json::parse(inp)
+        )
     );
 }
