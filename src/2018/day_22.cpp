84b42c18c12c5a68fbf9ede1be05b0124eb6f775
joe wezorek <jwezorek@gmail.com>
2024-08-04 14:27:44 -0700

    2018 day 22

--- src/2018/day_22.cpp
+++ src/2018/day_22.cpp
@@ -7,6 +7,8 @@
 #include <print>
 #include <ranges>
 #include <unordered_set>
+#include <set>
+#include <boost/functional/hash.hpp>
 
 namespace r = std::ranges;
 namespace rv = std::ranges::views;
@@ -106,6 +108,147 @@ namespace {
             std::plus<>()
         );
     }
+
+    enum tool {
+        neither = 0,
+        torch,
+        climbing_gear
+    };
+
+    auto other_tools(tool t) {
+        return rv::iota(0, 3) | rv::transform(
+            [](auto i)->tool {
+                return static_cast<tool>(i);
+            }
+        ) | rv::filter(
+            [t](auto other) {
+                return other != t;
+            }
+        );
+    }
+
+    struct traversal_state {
+        point loc;
+        tool tool_equipped;
+
+        bool operator==(const traversal_state& ts) const {
+            return loc == ts.loc && tool_equipped == ts.tool_equipped;
+        }
+    };
+
+    struct traversal_state_hash {
+        size_t operator()(const traversal_state& ts) const {
+            size_t seed = 0;
+            boost::hash_combine(seed, ts.loc.x);
+            boost::hash_combine(seed, ts.loc.y);
+            boost::hash_combine(seed, ts.tool_equipped);
+            return seed;
+        }
+    };
+
+    const std::set<tool>& tools_allowed_in_cave(cave_type ct) {
+        static const std::unordered_map<cave_type, std::set<tool>> tbl = {
+            {rocky, {climbing_gear, torch} },
+            {wet, {climbing_gear, neither}},
+            {narrow, {torch, neither}}
+        };
+        return tbl.at(ct);
+    }
+
+    auto adjacent_locs(const point& loc, const grid<cave_type>& cavern) {
+        auto [wd, hgt] = cavern.dimensions();
+        const static std::array<point, 4> deltas = {{
+            {0,-1},{1,0},{0,1},{-1,0}
+        }};
+        return deltas | rv::transform(
+            [loc](const auto& delta) {
+                return loc + delta;
+            }
+        ) | rv::filter(
+            [wd, hgt](auto&& pt) {
+                return pt.x >= 0 && pt.y >= 0 && pt.x < wd && pt.y < hgt;
+            }
+        );
+    }
+
+    std::vector<std::tuple<traversal_state,int>> next_moves(const grid<cave_type>& cavern, const traversal_state& ts) {
+        std::vector<std::tuple<traversal_state, int>> moves;
+        for (auto&& next_loc : adjacent_locs(ts.loc, cavern)) {
+            const auto& tools_allowed_there = tools_allowed_in_cave(cavern(next_loc));
+            if (tools_allowed_there.contains(ts.tool_equipped)) {
+                moves.emplace_back(
+                    traversal_state{ next_loc, ts.tool_equipped },
+                    1
+                );
+            }
+        }
+        const auto& tools_allowed_here = tools_allowed_in_cave(cavern(ts.loc));
+        for (auto new_tool : other_tools(ts.tool_equipped)) {
+            if (tools_allowed_here.contains(new_tool)) {
+                moves.emplace_back(
+                    traversal_state{ ts.loc, new_tool },
+                    7
+                );
+            }
+        }
+        return moves;
+    }
+
+    using time_map = std::unordered_map<traversal_state, int, traversal_state_hash>;
+
+    int lowest_time(const time_map& map, const point& target) {
+        return r::min(
+            map | rv::filter(
+                [target](auto&& pair) {
+                    const auto& [state, time] = pair;
+                    return state.loc == target && state.tool_equipped == torch;
+                }
+            ) | rv::values
+        );
+    }
+
+    int lowest_time_path(const grid<cave_type>& cavern, const point& target) {
+
+        time_map elapsed_time;
+
+        aoc::priority_queue<traversal_state, traversal_state_hash> queue;
+        traversal_state start_state = { {0, 0}, torch };
+        elapsed_time[start_state] = 0;
+        queue.insert(start_state, 0);
+
+        while (!queue.empty()) {
+            auto u = queue.extract_min();
+            auto moves = next_moves(cavern, u);
+
+            for (auto [v, time_to_v] : moves) {
+                auto dist_to_u = elapsed_time.at(u);
+                auto dist_through_u_to_v = dist_to_u + time_to_v;
+                auto curr_dist_to_v = elapsed_time.contains(v) ?
+                    elapsed_time.at(v) :
+                    std::numeric_limits<int>::max();
+
+                if (dist_through_u_to_v < curr_dist_to_v) {
+                    elapsed_time[v] = dist_through_u_to_v;
+                    if (queue.contains(v)) {
+                        queue.change_priority(v, dist_through_u_to_v);
+                    } else {
+                        queue.insert(v, dist_through_u_to_v);
+                    }
+                }
+            }
+        }
+
+        return lowest_time(elapsed_time, target);
+    }
+
+    int do_part_2(const scan_results& scan) {
+        const int k_padding = 250;
+        auto cave_system = build_cave_system(
+            scan, scan.target.x + k_padding, 
+            scan.target.y + k_padding
+        );
+        return lowest_time_path(cave_system, scan.target);
+    }
 }
 
 void aoc::y2018::day_22(const std::string& title) {
@@ -116,11 +259,8 @@ void aoc::y2018::day_22(const std::string& title) {
         )
     );
 
-    //scan.depth = 510;
-    //scan.target = { 10,10 };
-
     std::println("--- Day 22: {} ---", title);
     std::println("  part 1: {}", do_part_1(scan) );
-    std::println("  part 2: {}", 0);
+    std::println("  part 2: {}", do_part_2(scan) );
     
 }
