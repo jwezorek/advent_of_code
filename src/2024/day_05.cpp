ff242ccbff0e552463f53184f1e7a5730d71738b
joe wezorek <jwezorek@gmail.com>
2024-12-05 06:39:01 -0800

    changed my names to match the challege description

--- src/2024/day_05.cpp
+++ src/2024/day_05.cpp
@@ -17,11 +17,11 @@ namespace rv = std::ranges::views;
 
 namespace {
 
-    using edge = std::tuple<int, int>;
+    using rule = std::tuple<int, int>;
     using graph = std::unordered_map<int, std::unordered_set<int>>;
-    using rule = std::vector<int>;
+    using update = std::vector<int>;
 
-    graph edges_to_graph(const std::vector<edge>& edges) {
+    graph rules_to_graph(const std::vector<rule>& edges) {
         graph g;
         for (const auto& [u, v] : edges) {
             g[u].insert(v);
@@ -29,12 +29,12 @@ namespace {
         return g;
     }
 
-    std::tuple<graph, std::vector<rule>> parse_input(const std::vector<std::string>& inp) {
+    std::tuple<graph, std::vector<update>> parse_input(const std::vector<std::string>& inp) {
         auto sections = aoc::group_strings_separated_by_blank_lines(inp);
         return {
-            edges_to_graph(
+            rules_to_graph(
                 sections.front() | rv::transform(
-                    [](auto&& str)->edge {
+                    [](auto&& str)->rule {
                         auto verts = aoc::extract_numbers(str);
                         return { verts[0], verts[1] };
                     }
@@ -48,8 +48,8 @@ namespace {
         };
     }
 
-    bool is_valid_rule(const graph& g, const rule& rule) {
-        for (auto [i, j] : rule | rv::pairwise) {
+    bool is_valid_update(const graph& g, const update& update) {
+        for (auto [i, j] : update | rv::pairwise) {
             if (g.at(j).contains(i)) {
                 return false;
             }
@@ -57,11 +57,11 @@ namespace {
         return true;
     }
 
-    int do_part_1(const graph& g, const std::vector<rule>& rules) {
+    int do_part_1(const graph& g, const std::vector<update>& updates) {
         return r::fold_left(
-            rules | rv::transform(
+            updates | rv::transform(
                 [&](auto&& rule) {
-                    return is_valid_rule(g, rule) ? rule[rule.size() / 2] : 0;
+                    return is_valid_update(g, rule) ? rule[rule.size() / 2] : 0;
                 }
             ),
             0,
@@ -69,11 +69,11 @@ namespace {
         );
     }
 
-    int do_part_2(const graph& g, const std::vector<rule>& rules) {
+    int do_part_2(const graph& g, const std::vector<update>& updates) {
         return r::fold_left(
-            rules | rv::transform(
+            updates | rv::transform(
                 [&](auto rule) {
-                    if (is_valid_rule(g, rule)) {
+                    if (is_valid_update(g, rule)) {
                         return 0;
                     }
                     r::sort(
@@ -93,14 +93,14 @@ namespace {
 
 void aoc::y2024::day_05(const std::string& title) {
 
-    auto [graph, rules] = parse_input(
+    auto [graph, updates] = parse_input(
         aoc::file_to_string_vector(
             aoc::input_path(2024, 5)
         )
     );
 
     std::println("--- Day 5: {} ---", title);
-    std::println("  part 1: {}", do_part_1(graph, rules));
-    std::println("  part 2: {}", do_part_2(graph, rules));
+    std::println("  part 1: {}", do_part_1(graph, updates));
+    std::println("  part 2: {}", do_part_2(graph, updates));
     
 }
