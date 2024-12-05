cbebfd8f709c17749a6e7148cdcc9e7b44df4d0e
joe wezorek <jwezorek@gmail.com>
2024-12-05 09:48:34 -0800

    renamed graph to rules

--- src/2024/day_05.cpp
+++ src/2024/day_05.cpp
@@ -18,18 +18,18 @@ namespace rv = std::ranges::views;
 namespace {
 
     using rule = std::tuple<int, int>;
-    using graph = std::unordered_map<int, std::unordered_set<int>>;
+    using rules = std::unordered_map<int, std::unordered_set<int>>;
     using update = std::vector<int>;
 
-    graph rules_to_graph(const std::vector<rule>& edges) {
-        graph g;
+    rules rules_to_graph(const std::vector<rule>& edges) {
+        rules g;
         for (const auto& [u, v] : edges) {
             g[u].insert(v);
         }
         return g;
     }
 
-    std::tuple<graph, std::vector<update>> parse_input(const std::vector<std::string>& inp) {
+    std::tuple<rules, std::vector<update>> parse_input(const std::vector<std::string>& inp) {
         auto sections = aoc::group_strings_separated_by_blank_lines(inp);
         return {
             rules_to_graph(
@@ -48,7 +48,7 @@ namespace {
         };
     }
 
-    bool is_valid_update(const graph& g, const update& update) {
+    bool is_valid_update(const rules& g, const update& update) {
         for (auto [i, j] : update | rv::pairwise) {
             if (g.at(j).contains(i)) {
                 return false;
@@ -57,7 +57,7 @@ namespace {
         return true;
     }
 
-    int do_part_1(const graph& g, const std::vector<update>& updates) {
+    int do_part_1(const rules& g, const std::vector<update>& updates) {
         return r::fold_left(
             updates | rv::transform(
                 [&](auto&& rule) {
@@ -69,7 +69,7 @@ namespace {
         );
     }
 
-    int do_part_2(const graph& g, const std::vector<update>& updates) {
+    int do_part_2(const rules& g, const std::vector<update>& updates) {
         return r::fold_left(
             updates | rv::transform(
                 [&](auto rule) {
@@ -93,14 +93,14 @@ namespace {
 
 void aoc::y2024::day_05(const std::string& title) {
 
-    auto [graph, updates] = parse_input(
+    auto [rules, updates] = parse_input(
         aoc::file_to_string_vector(
             aoc::input_path(2024, 5)
         )
     );
 
     std::println("--- Day 5: {} ---", title);
-    std::println("  part 1: {}", do_part_1(graph, updates));
-    std::println("  part 2: {}", do_part_2(graph, updates));
+    std::println("  part 1: {}", do_part_1(rules, updates));
+    std::println("  part 2: {}", do_part_2(rules, updates));
     
 }
