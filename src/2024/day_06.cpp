61b7961fbe056305c8312f7d9a7a1d13e2956ed6
joe wezorek <jwezorek@gmail.com>
2024-12-05 22:02:50 -0800

    clean up

--- src/2024/day_06.cpp
+++ src/2024/day_06.cpp
@@ -43,86 +43,84 @@ namespace {
         int hgt;
     };
 
-    std::tuple<point, bounds, point_set, point_set> parse_input(const grid& grid) {
-        bounds bounds{ static_cast<int>(grid.front().size()), static_cast<int>(grid.size()) };
+    std::tuple<point, bounds, point_set> parse_input(const grid& grid) {
+        bounds bnds{ static_cast<int>(grid.front().size()), static_cast<int>(grid.size()) };
         point start;
         point_set walls;
-        point_set floors;
-        for (auto [x, y] : rv::cartesian_product(rv::iota(0, bounds.wd), rv::iota(0, bounds.hgt))) {
+        for (auto [x, y] : rv::cartesian_product(rv::iota(0, bnds.wd), rv::iota(0, bnds.hgt))) {
             auto pt = point{ x,y };
             auto tile = grid[y][x];
             if (tile == '^') {
                 start = pt;
             } else if (tile == '#') {
                 walls.insert(pt);
-            } else {
-                floors.insert(pt);
-            }
+            } 
         }
-        return { start, bounds, std::move(walls), std::move(floors) };
+        return { start, bnds, std::move(walls) };
     }
 
     bool in_bounds(const point& loc, const bounds& bounds) {
         return loc.x >= 0 && loc.y >= 0 && loc.x < bounds.wd && loc.y < bounds.hgt;
     }
 
-    point nth_direction(int n) {
-        static const std::array<point, 4> dirs = { {
-            {0,-1}, {1,0}, {0,1}, {-1,0}
-        } };
-        return dirs[n];
-    }
-
-    bool is_facing_wall(const point& loc, int dir, const point_set& walls) {
-        return walls.contains(loc + nth_direction(dir));
-    }
-
     bool simulate_guard(const point& start, const bounds& bounds, 
-        const point_set& walls, std::function<void(const state&)> on_visit = {}) {
+            const point_set& walls, std::function<void(const state&)> on_visit = {}) {
+
+        static const std::array<point, 4> dirs = {{
+            {0,-1}, {1,0}, {0,1}, {-1,0}
+        }};
+
         state s = { start, 0 };
         state_set visited;
+
         while (in_bounds(s.loc, bounds) && !visited.contains(s)) {
             visited.insert(s);
             if (on_visit) {
                 on_visit(s);
             }
-            if (is_facing_wall(s.loc, s.dir, walls)) {
+            // if facing a wall, turn right...
+            if (walls.contains(s.loc + dirs.at(s.dir))) {
                 s.dir = (s.dir + 1) % 4;
                 continue;
             }
-            s.loc = s.loc + nth_direction(s.dir);
+            s.loc = s.loc + dirs.at(s.dir);
         }
         return visited.contains(s);
     }
 
-    int count_visited_locs(
-            const point& start, const bounds& bounds, const point_set& walls) {
+    point_set visited_locs(const point& start, const bounds& bounds, const point_set& walls) {
         point_set visited;
         simulate_guard(start, bounds, walls,
             [&visited](const state& s) {
                 visited.insert(s.loc);
             }
         );
-        return static_cast<int>(visited.size());
+        return visited;
     }
 
-    int count_loops(const point& start, const bounds& bounds, 
-            const point_set& walls, const point_set& floors) {
-        int count = 0;
-        for (auto loc : floors) {
-            auto new_walls = walls;
-            new_walls.insert(loc);
-            if (simulate_guard(start, bounds, new_walls)) {
-                ++count;
+    int count_visited_locs(
+            const point& start, const bounds& bounds, const point_set& walls) {
+        return static_cast<int>(
+            visited_locs(start, bounds, walls).size()
+        );
+    }
+
+    int count_loops(const point& start, const bounds& bounds, const point_set& walls) {
+        auto floors = visited_locs(start, bounds, walls);
+        return r::count_if(
+            floors,
+            [&](auto&& loc) {
+                auto new_walls = walls;
+                new_walls.insert(loc);
+                return simulate_guard(start, bounds, new_walls);
             }
-        }
-        return count;
+        );
     }
 }
 
 void aoc::y2024::day_06(const std::string& title) {
 
-    auto [start_loc, bounds, walls, floors] = parse_input(
+    auto [start_loc, bounds, walls] = parse_input(
         aoc::file_to_string_vector(
             aoc::input_path(2024, 6)
         )
@@ -130,6 +128,6 @@ void aoc::y2024::day_06(const std::string& title) {
 
     std::println("--- Day 6: {} ---", title);
     std::println("  part 1: {}", count_visited_locs(start_loc, bounds, walls));
-    std::println("  part 2: {}", count_loops(start_loc, bounds, walls, floors));
+    std::println("  part 2: {}", count_loops(start_loc, bounds, walls));
     
 }
