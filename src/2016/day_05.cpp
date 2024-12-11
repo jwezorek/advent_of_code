65924541ec4aed60f7db9a332748d7936fbc673f
joe wezorek <jwezorek@gmail.com>
2024-12-11 09:44:43 -0800

    2016 day 5

--- src/2016/day_05.cpp
+++ src/2016/day_05.cpp
@@ -1,5 +1,5 @@
-
 #include "../util/util.h"
+#include "../2015/md5.h"
 #include "y2016.h"
 #include <filesystem>
 #include <functional>
@@ -13,17 +13,59 @@ namespace rv = std::ranges::views;
 /*------------------------------------------------------------------------------------------------*/
 
 namespace {
+    std::string find_password(const std::string& door_id) {
 
+        std::stringstream output;
+        uint64_t key_num = 0;
+        int pwd_char_count = 0;
+
+        while (pwd_char_count < 8) {
+            std::string key = door_id + std::to_string(key_num++);
+            auto hash = md5(key);
+            if (hash.substr(0, 5) == "00000") {
+                output << hash[5];
+                ++pwd_char_count;
+            }
+        }
+
+        return output.str();
+    }
+
+    std::string find_password_with_positions(const std::string& door_id) {
+
+        uint64_t key_num = 0;
+        int pwd_char_count = 0;
+        std::array<char, 8> pwd = { {0,0,0,0,0,0,0,0} };
+
+        while (pwd_char_count < 8) {
+            std::string key = door_id + std::to_string(key_num++);
+            auto hash = md5(key);
+            if (hash.substr(0, 5) == "00000") {
+                auto pos_char = hash[5];
+                if (pos_char < '0' || pos_char > '7') {
+                    continue;
+                }
+                int pos = pos_char - '0';
+                if (pwd[pos] != 0) {
+                    continue;
+                }
+                pwd[pos] = hash[6];
+                pwd_char_count++;
+            }
+        }
+
+        return pwd | r::to<std::string>();
+    }
 }
 
 void aoc::y2016::day_05(const std::string& title) {
 
-    auto inp = aoc::file_to_string_vector(
-            aoc::input_path(2016, 5)
-        ); 
+    auto door_id = aoc::file_to_string(
+        aoc::input_path(2016, 5)
+    );
 
     std::println("--- Day 5: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
-    
+    std::println("  part 1: {}", find_password(door_id));
+    std::println("  part 2: {}", find_password_with_positions(door_id));
+
 }
