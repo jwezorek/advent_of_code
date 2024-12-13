fafee30a1cf0a0ef6b022f451fac9e2a04858eb2
joe wezorek <jwezorek@gmail.com>
2024-12-12 21:50:54 -0800

    2024 day 13 part 1 naive

--- src/2024/day_13.cpp
+++ src/2024/day_13.cpp
@@ -1,5 +1,6 @@
 
 #include "../util/util.h"
+#include "../util/vec2.h"
 #include "y2024.h"
 #include <filesystem>
 #include <functional>
@@ -14,16 +15,65 @@ namespace rv = std::ranges::views;
 
 namespace {
 
+    using vec2 = aoc::vec2<int>;
+
+    struct claw_game {
+        vec2 button_a;
+        vec2 button_b;
+        vec2 prize;
+    };
+
+    claw_game parse_claw_game(const std::vector<std::string>& inp) {
+        auto vals = inp | rv::transform(
+                [](auto&& str) {
+                    return aoc::extract_numbers(str);
+                }
+            ) | r::to<std::vector>();
+        return {
+            {vals[0][0], vals[0][1]},
+            {vals[1][0], vals[1][1]},
+            {vals[2][0], vals[2][1]}
+        };
+    }
+
+    std::optional<int> fewest_tokens_to_win(const claw_game& cg) {
+        std::optional<int> fewest_tokens = {};
+        for (auto [a_clicks, b_clicks] : rv::cartesian_product(rv::iota(0, 100), rv::iota(0, 100))) {
+            auto loc = a_clicks * cg.button_a + b_clicks * cg.button_b;
+            if (loc == cg.prize) {
+                int tokens = 3 * a_clicks + b_clicks;
+                if (!fewest_tokens || tokens < *fewest_tokens) {
+                    fewest_tokens = tokens;
+                }
+            }
+        }
+        return fewest_tokens;
+    }
+
+    int do_part_1(const std::vector<claw_game>& games) {
+        int fewest_tokens = 0;
+        for (const auto& game : games) {
+            auto toks = fewest_tokens_to_win(game);
+            if (toks) {
+                fewest_tokens += *toks;
+            }
+        }
+        return fewest_tokens;
+    }
 }
 
 void aoc::y2024::day_13(const std::string& title) {
 
-    auto inp = aoc::file_to_string_vector(
-            aoc::input_path(2024, 13)
-        ); 
-
+    auto inp = aoc::group_strings_separated_by_blank_lines(
+            aoc::file_to_string_vector(
+                aoc::input_path(2024, 13)
+            )
+        ) | rv::transform(
+            parse_claw_game
+        ) | r::to<std::vector>();
+ 
     std::println("--- Day 13: {} ---", title);
-    std::println("  part 1: {}", 0);
+    std::println("  part 1: {}", do_part_1(inp) );
     std::println("  part 2: {}", 0);
     
 }
--- src/2024/y2024.cpp
+++ src/2024/y2024.cpp
@@ -18,7 +18,7 @@ void aoc::y2024::do_advent_of_code(int day) {
         []() { day_10( "Hoof It" ); },
         []() { day_11( "Plutonian Pebbles" ); },
         []() { day_12( "Garden Groups" ); },
-        []() { day_13( "<unknown>" ); },
+        []() { day_13( "Claw Contraption" ); },
         []() { day_14( "<unknown>" ); },
         []() { day_15( "<unknown>" ); },
         []() { day_16( "<unknown>" ); },
