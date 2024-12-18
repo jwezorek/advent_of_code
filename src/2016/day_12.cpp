4cb637aac63621b4fe697242ae0791e1e4f0f0cd
joe wezorek <jwezorek@gmail.com>
2024-12-18 10:20:57 -0800

    2016 day 12

--- src/2016/day_12.cpp
+++ src/2016/day_12.cpp
@@ -22,7 +22,7 @@ namespace {
         jnz
     };
 
-    using operand = std::variant<int64_t, char>;
+    using operand = std::variant<std::monostate, int64_t, char>;
 
     struct statement {
         op op;
@@ -32,6 +32,13 @@ namespace {
     struct state {
         int instr_ptr;
         std::unordered_map<char, int64_t> registers;
+
+        state() : instr_ptr(0) {
+            registers['a'] = 0;
+            registers['b'] = 0;
+            registers['c'] = 0;
+            registers['d'] = 0;
+        }
     };
 
     operand str_to_operand(const std::string& str) {
@@ -62,6 +69,84 @@ namespace {
             ) | r::to<std::vector>()
         };
     }
+
+    int64_t eval_operand(const state& state, const operand& operand) {
+        return std::visit(
+            aoc::overload{
+                [&](char reg)->int64_t {
+                    return state.registers.at(reg);
+                },
+                [](int64_t val)->int64_t {
+                    return val;
+                },
+                [](std::monostate)->int64_t {
+                    throw std::runtime_error("bad operand");
+                }
+            },
+            operand
+        );
+    }
+
+    int64_t& eval_operand_as_ref(state& state, const operand& operand) {
+        return state.registers[std::get<char>(operand)];
+    }
+
+    using op_fn = std::function<void(state&, const operand&, const operand&)>;
+
+    void do_inc(state& state, const operand& x, const operand& y) {
+        ++eval_operand_as_ref(state, x);
+    }
+
+    void do_dec(state& state, const operand& x, const operand& y) {
+        --eval_operand_as_ref(state, x);
+    }
+
+    void do_jnz(state& state, const operand& x, const operand& y) {
+        if (eval_operand(state, x) != 0) {
+            state.instr_ptr += eval_operand(state, y);
+        }
+    }
+
+    void do_cpy(state& state, const operand& x, const operand& y) {
+        eval_operand_as_ref(state, y) = eval_operand(state, x);
+    }
+
+    void exec_statement(state& state, const statement& stmt) {
+        const static std::unordered_map<op, op_fn> op_tbl = {
+            {cpy, do_cpy},
+            {inc, do_inc},
+            {dec, do_dec},
+            {jnz, do_jnz}
+        };
+
+        auto arg1 = stmt.args[0];
+        auto arg2 = (stmt.args.size() > 1) ? stmt.args[1] : operand{};
+        op_tbl.at(stmt.op)( state, arg1, arg2);
+    }
+
+    state run_program(const state& inp, const std::vector<statement>& program) {
+        state state = inp;
+        while (state.instr_ptr < program.size()) {
+            auto old_instr_ptr = state.instr_ptr;
+            exec_statement(state, program.at(state.instr_ptr));
+            if (state.instr_ptr == old_instr_ptr) {
+                ++state.instr_ptr;
+            }
+        }
+        return state;
+    }
+
+    int do_part_1(const std::vector<statement>& program) {
+        auto state = run_program({},program);
+        return state.registers['a'];
+    }
+
+    int do_part_2(const std::vector<statement>& program) {
+        state inp;
+        inp.registers['c'] = 1;
+        auto state = run_program(inp, program);
+        return state.registers['a'];
+    }
 }
 
 void aoc::y2016::day_12(const std::string& title) {
@@ -73,7 +158,7 @@ void aoc::y2016::day_12(const std::string& title) {
         ) | r::to<std::vector>();
 
     std::println("--- Day 12: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", do_part_1(inp) );
+    std::println("  part 2: {}", do_part_2(inp) );
     
 }
