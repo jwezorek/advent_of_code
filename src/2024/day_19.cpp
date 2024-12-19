331062020cb59575ae28c2bc6de73c14c64ffa62
joe wezorek <jwezorek@gmail.com>
2024-12-19 09:53:04 -0800

    2024 day 19

--- src/2024/day_19.cpp
+++ src/2024/day_19.cpp
@@ -9,6 +9,7 @@
 #include <unordered_map>
 #include <memory>
 #include <stack>
+#include <string_view>
 
 namespace r = std::ranges;
 namespace rv = std::ranges::views;
@@ -27,104 +28,70 @@ namespace {
         };
     }
 
-    int to_index(char color) {
-        static const std::unordered_map<char, int> to_index_tbl = {
-            {'w',0},
-            {'u',1},
-            {'b',2},
-            {'r',3},
-            {'g',4}
-        };
-        return to_index_tbl.at(color);
+    auto next_states(const strings& words, size_t i, const std::string& str) {
+        return words |
+            rv::filter(
+                [&str,i](const auto& word)->bool {
+                    if (word.size() > str.size() - i) {
+                        return false;
+                    }
+                    auto beg = str.begin();
+                    std::string_view sv(&*(str.begin() + i), word.size());
+                    return sv == word;
+                }
+            ) | rv::transform(
+                [=](auto&& word)->size_t {
+                    return i + word.size();
+                }
+            );
     }
 
-    struct trie {
-        char ch;
-        std::array<std::shared_ptr<trie>, 5> children;
-        bool is_terminal;
+    bool test_design(const strings& words, const std::string& str) {
+        std::unordered_set<size_t> visited;
+        std::stack<size_t> stack;
+        stack.push( 0 );
 
-        trie() : ch(0), is_terminal(false) {
-        }
-    };
-
-    using  str_iter = std::string::const_iterator;
-
-    void insert_into_trie(trie& tri, str_iter& i, str_iter end) {
-        if (i == end) {
-            return;
-        }
-        auto ch = *i;
-        auto ch_index = to_index(ch);
-        if (! tri.children[ch_index]) {
-            tri.children[ch_index] = std::make_shared<trie>();
-        }
-        auto& next_trie_node = *tri.children[ch_index];
-        next_trie_node.ch = ch;
-        next_trie_node.is_terminal = (next_trie_node.is_terminal || std::next(i) == end);
-
-        ++i;
-        insert_into_trie(next_trie_node, i, end);
-    }
-
-    void insert_into_trie(trie& trie, const std::string& str) {
-        str_iter i = str.begin();
-        insert_into_trie(trie, i, str.end());
-    }
-
-    bool has_word(const trie& tri, str_iter& i, str_iter end) {
-        if (i == end) {
-            return tri.is_terminal;
-        }
-        auto ch = *i;
-        auto ch_index = to_index(ch);
-        if (tri.children[ch_index]) {
-            ++i;
-            return has_word(*tri.children[ch_index], i, end);
-        }
-        return false;
-    }
-
-    bool has_word(const trie& tri, const std::string& str) {
-        str_iter i = str.begin();
-        return has_word(tri, i, str.end());
-    }
-
-    struct state {
-        int i;
-        const trie* trie_node;
-    };
-
-    bool test_design(const trie& tri, const std::string& str) {
-        std::stack<state> stack;
-        stack.emplace(
-            0,
-            &tri
-        );
         while (!stack.empty()) {
             auto curr_state = stack.top();
             stack.pop();
 
-            if (curr_state.i == str.size()) {
+            if (curr_state == str.size()) {
                 return true;
             }
 
-            if (curr_state.trie_node->is_terminal) {
-                stack.emplace(
-                    curr_state.i+1,
-                    &tri
-                );
+            if (visited.contains(curr_state)) {
+                continue;
             }
-
-            auto ch = str[curr_state.i];
-            if (curr_state.trie_node->children[to_index(ch)]) {
-                stack.emplace(
-                    curr_state.i + 1,
-                    curr_state.trie_node->children[to_index(ch)].get()
-                );
+            visited.insert(curr_state);
+            for (auto next : next_states(words, curr_state, str)) {
+                stack.push(next);
             }
         }
 
         return false;
+    }
+
+    uint64_t count_all_paths(std::unordered_map<size_t, uint64_t>& count, const strings& words, size_t i, const std::string& str) {
+        if (i == str.size()) {
+            return 1;
+        }
+
+        uint64_t sum = 0;
+        for (auto j : next_states(words, i, str)) {
+            if (count.contains(j)) {
+                sum += count[j];
+                continue;
+            }
+            sum += count_all_paths(count, words, j, str);
+        }
+
+        count[i] = sum;
+        return sum;
+    }
+
+    uint64_t count_all_paths(const strings& words, const std::string& str) {
+        std::unordered_map<size_t, uint64_t> count;
+        return count_all_paths(count, words, 0, str);
     }
 }
 
@@ -136,22 +103,27 @@ void aoc::y2024::day_19(const std::string& title) {
         )
     );
 
-    trie tri;
-    for (const auto& towel : towels) {
-        insert_into_trie(tri, towel);
-    }
-
-    std::println("u => {}", test_design(tri, "u"));
-
     std::println("--- Day 19: {} ---", title);
+
     std::println("  part 1: {}",
         r::count_if(
             designs,
-            [&](const std::string& str) {
-                return test_design(tri, str);
+            [&](const std::string& design) {
+                return test_design(towels, design);
             }
         )
     );
-    std::println("  part 2: {}", 0);
+
+    std::println("  part 2: {}",
+        r::fold_left(
+            designs | rv::transform(
+                [&](auto&& design) {
+                    return count_all_paths(towels, design);
+                }
+            ),
+            0,
+            std::plus<>()
+        )
+    );
     
 }
