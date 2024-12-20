118ccc7f1245e51d5f3dc28a339db9e1b14b78c3
joe wezorek <jwezorek@gmail.com>
2024-12-20 10:01:44 -0800

    2024 day 20 both parts but part 2 has some bug

--- src/2024/day_20.cpp
+++ src/2024/day_20.cpp
@@ -21,6 +21,9 @@ namespace {
     using point = aoc::vec2<int>;
     using point_set = aoc::vec2_set<int>;
 
+    template<typename T>
+    using point_map = aoc::vec2_map<int, T>;
+
     struct racetrack {
         point start;
         point end;
@@ -53,28 +56,11 @@ namespace {
         point cheat2;
     };
 
-    struct state {
-        point loc;
-        bool used_cheat;
-
-        bool operator==(const state& s) const {
-            return loc == s.loc && used_cheat == s.used_cheat;
-        }
-    };
-
-    struct hash_state {
-        size_t operator()(const state& s) const {
-            size_t seed = aoc::hash_vec2<int>{}(s.loc);
-            boost::hash_combine(seed, s.used_cheat);
-            return seed;
-        }
-    };
-
     bool in_bounds(const point& loc, int wd, int hgt) {
         return loc.x >= 0 && loc.y >= 0 && loc.x < wd && loc.y < hgt;
     }
 
-    std::vector<cheat> find_cheats(const racetrack& rt) {
+    std::vector<cheat> find_simple_cheats(const racetrack& rt) {
         const static std::array<point, 4> deltas = { {
             {0,-1},{1,0},{0,1},{-1,0}
         } };
@@ -95,127 +81,144 @@ namespace {
         return cheats;
     }
 
-    std::vector<state> next_states(const state& s, const racetrack& rt, std::optional<cheat> cheat) {
-
-        if (s.used_cheat && cheat) {
-            if (s.loc == cheat->cheat1) {
-                return  std::vector<state>{ {cheat->cheat2, true} };
-            }
-        }
-
-        const static std::array<point, 4> deltas = {{
-            {0,-1},{1,0},{0,1},{-1,0}
-        }};
-
-        auto next_states = deltas | rv::transform(
-            [&](auto&& delta)->state {
-                return { delta + s.loc, s.used_cheat };
+    auto adjacent_locs(const racetrack& rt, const point& loc) {
+        const static std::array<point, 4> deltas = { { {0,-1},{1,0},{0,1},{-1,0} } };
+        return deltas | rv::transform(
+            [loc](auto&& delta) {
+                return loc + delta;
             }
         ) | rv::filter(
-            [&](auto&& new_state) {
-                return !rt.walls.contains(new_state.loc);
-            }
-        ) | r::to<std::vector>();
-
-        if (!s.used_cheat && cheat) {
-            for (auto delta : deltas) {
-                auto adj = s.loc + delta;
-                if (adj == cheat->cheat1) {
-                    next_states.emplace_back(
-                        adj,
-                        true
-                    );
-                }
-            }
-        }
-
-        return next_states;
-    }
-
-    using state_set = std::unordered_set<state, hash_state>;
-
-    int shortest_path(const racetrack& rt, std::optional<cheat> cheat) {
-        std::queue<std::tuple<state, int>> queue;
-        state_set visited;
-
-        queue.emplace(state{ rt.start,false }, 0);
-        while (!queue.empty()) {
-            auto [state, dist] = queue.front();
-            queue.pop();
-
-            if (state.loc == rt.end) {
-                return dist;
-            }
-
-            if (visited.contains(state)) {
-                continue;
-            }
-            visited.insert(state);
-
-            for (auto next_state : next_states(state, rt, cheat)) {
-                queue.emplace(next_state, dist+1);
-            }
-        }
-
-        return -1;
-    }
-
-    int do_part_1(const racetrack& rt) {
-        auto no_cheats = shortest_path(rt, {});
-        auto cheats = find_cheats(rt);
-        return std::count_if(
-            std::execution::par,
-            cheats.begin(),
-            cheats.end(),
-            [&](auto&& cheat)->bool {
-                auto with_cheat = shortest_path(rt, cheat);
-                return (no_cheats - with_cheat >= 100);
+            [&rt](auto&& new_loc) {
+                return !rt.walls.contains(new_loc);
             }
         );
     }
 
-    int64_t do_part_2(const racetrack& rt) {
-        auto empty_locs = rv::cartesian_product(rv::iota(0, rt.wd), rv::iota(0, rt.hgt)) |
-            rv::transform(
-                [&](auto&& tup)->point {
-                    const auto& [x, y] = tup;
-                    return { x,y };
+    point_map<int> shortest_path_map( const racetrack& rt, bool start_to_end ) {
+
+        std::queue<std::tuple<point, int>> queue;
+        point_map<int> shortest_path;
+
+        point src, dest;
+        std::tie(src, dest) = (start_to_end) ?
+            std::tuple<point, point>{ rt.start, rt.end} :
+            std::tuple<point, point>{ rt.end,rt.start };
+
+        queue.emplace(src, 0);
+        while (!queue.empty()) {
+            auto [loc, dist] = queue.front();
+            queue.pop();
+
+            if (shortest_path.contains(loc)) {
+                continue;
+            }
+            shortest_path[loc] = dist;
+            
+            for (auto adj : adjacent_locs(rt, loc)) {
+                queue.emplace(adj, dist+1);
+            }
+        }
+
+        return shortest_path;
+    }
+
+    int manhattan_distance(const point& u, const point& v) {
+        return std::abs(v.x - u.x) + std::abs(v.y - u.y);
+    }
+
+    int shortest_path_with_cheat(const point_map<int>& from_start, const point_map<int>& to_end, cheat cheat) {
+        const static std::array<point, 4> deltas = { { {0,-1},{1,0},{0,1},{-1,0} } };
+        auto entrance_to_cheat = r::min(
+            deltas | rv::transform(
+                [&](auto&& delta) {
+                    return cheat.cheat1 + delta;
                 }
             ) | rv::filter(
                 [&](auto&& pt) {
-                    return !rt.walls.contains(pt);
+                    return from_start.contains(pt);
                 }
-            ) | r::to<std::vector>();
-
-        auto internal_walls = rt.walls | rv::filter(
-                [&](auto&& wall) {
-                    return wall.x != 0 && wall.y != 0 && wall.x != rt.wd - 1 && wall.y != rt.hgt - 1;
-                }
-            ) | r::to<std::vector>();
-
-        auto cheats = rv::cartesian_product(internal_walls, empty_locs) | rv::filter(
-                [](auto&& pair) {
-                    const auto& [from, to] = pair;
-                    return std::abs(from.x - to.x) + std::abs(from.y - to.y) + 1 <= 20;
-                }
-            ) | rv::transform(
-                [](auto&& pair)->cheat {
-                    const auto& [from, to] = pair;
-                    return { from,to };
-                }
-            ) | r::to<std::vector>();
-
-        auto no_cheats = shortest_path(rt, {});
-
-        return std::count_if(
-            std::execution::par,
-            cheats.begin(),
-            cheats.end(),
-            [&](auto&& cheat)->bool {
-                auto with_cheat = shortest_path(rt, cheat);
-                return (no_cheats - with_cheat >= 100);
+            ),
+            [&](auto&& lhs, auto&& rhs) {
+                return from_start.at(lhs) < from_start.at(rhs);
             }
         );
+        return from_start.at(entrance_to_cheat) +
+            manhattan_distance(cheat.cheat1, cheat.cheat2) +
+            to_end.at(cheat.cheat2);
+    }
+
+    int64_t count_timesaving_cheats(
+            const racetrack& rt, const std::vector<cheat>& cheats, int min_saving) {
+
+        auto from_start = shortest_path_map(rt, true);
+        auto to_end = shortest_path_map(rt, false);
+
+        int without_cheating_len = from_start.at(rt.end);
+
+        return r::count_if(
+            cheats,
+            [&](auto&& cheat) {
+                auto len = shortest_path_with_cheat(
+                    from_start, to_end, cheat
+                );
+                return without_cheating_len - len >= min_saving;
+            }
+        );
+    }
+
+    std::vector<point> manhattan_dist_neighborhood(int sz) {
+        return rv::cartesian_product(rv::iota(-sz, sz + 1), rv::iota(-sz, sz + 1)) |
+            rv::transform(
+                [](auto&& tup)->point {
+                    auto [x, y] = tup;
+                    return { x,y };
+                }
+            ) | rv::filter(
+                [sz](auto&& pt) {
+                    if (pt == point{0, 0}) {
+                        return false;
+                    }
+                    return manhattan_distance({ 0,0 }, pt) < sz;
+                }
+            ) | r::to<std::vector>();
+    }
+
+    bool adjacent_to_empty_loc(const racetrack& rt, const point& loc) {
+        const static std::array<point, 4> deltas = {{ {0,-1},{1,0},{0,1},{-1,0} }};
+        for (const auto& delta : deltas) {
+            auto adj = loc + delta;
+            if (in_bounds(adj, rt.wd, rt.hgt) && !rt.walls.contains(adj)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    std::vector<cheat> find_cheats(const racetrack& rt, int max_dist) {
+        auto cheat_starts = rt.walls | rv::filter(
+            [&](auto&& wall) {
+                if (wall.x == 0 && wall.y == 0 && wall.x == rt.wd - 1 && wall.y == rt.hgt - 1) {
+                    return false;
+                }
+                return adjacent_to_empty_loc(rt, wall);
+            }
+        ) | r::to<std::vector>();
+
+        auto neighborhood = manhattan_dist_neighborhood(max_dist);
+
+        return rv::cartesian_product(
+            cheat_starts, neighborhood
+        ) | rv::transform(
+            [](auto&& pair)->cheat {
+                const auto& [start_loc, delta] = pair;
+                return { start_loc, start_loc + delta };
+            }
+        ) | rv::filter(
+            [&](auto&& cheat) {
+                return in_bounds(cheat.cheat2, rt.wd, rt.hgt) && 
+                    !rt.walls.contains(cheat.cheat2);
+            }
+        ) | r::to<std::vector>();
     }
 }
 
@@ -228,7 +231,7 @@ void aoc::y2024::day_20(const std::string& title) {
     );
 
     std::println("--- Day 20: {} ---", title);
-    std::println("  part 1: {}", do_part_1(inp) );
-    //std::println("  part 2: {}", do_part_2(inp) );
+    std::println("  part 1: {}", count_timesaving_cheats(inp, find_cheats(inp, 2), 100) );
+    std::println("  part 2: {}", count_timesaving_cheats(inp, find_cheats(inp, 20), 100) );
     
 }
