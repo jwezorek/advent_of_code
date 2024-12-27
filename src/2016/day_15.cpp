3ad0945e60a277de71c25ee12ed775b8dd874141
joe wezorek <jwezorek@gmail.com>
2024-12-27 12:13:11 -0800

    2016 day 15

--- src/2016/day_15.cpp
+++ src/2016/day_15.cpp
@@ -14,16 +14,57 @@ namespace rv = std::ranges::views;
 
 namespace {
 
+    struct disc {
+        int index;
+        int num_positions;
+        int starting_postion;
+    };
+
+    disc parse_disc(const std::string& str) {
+        auto parts = aoc::extract_numbers(str);
+        return {
+            parts[0],
+            parts[1],
+            parts[3]
+        };
+    }
+
+    int do_part_1(const std::vector<disc>& discs) {
+        int time = 0;
+        bool done = false;
+        do {
+            done = true;
+            for (const auto& disc : discs) {
+                if (((time + disc.index + disc.starting_postion) %
+                    disc.num_positions) != 0) {
+                    done = false;
+                    ++time;
+                    break;
+                }
+            }
+
+        } while (!done);
+
+        return time;
+    }
 }
 
 void aoc::y2016::day_15(const std::string& title) {
 
     auto inp = aoc::file_to_string_vector(
-            aoc::input_path(2016, 15)
-        ); 
+        aoc::input_path(2016, 15)
+    ) | rv::transform(
+        parse_disc
+    ) | r::to<std::vector>();
 
     std::println("--- Day 15: {} ---", title);
-    std::println("  part 1: {}", 0);
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", do_part_1(inp) );
+
+    inp.emplace_back(
+        inp.back().index + 1,
+        11,
+        0
+    );
+    std::println("  part 2: {}", do_part_1(inp) );
     
 }
