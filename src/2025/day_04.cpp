e60c7bd44fc45f5a684b37e8a1880addcc5b6653
joe wezorek <jwezorek@gmail.com>
2025-12-03 22:16:28 -0800

    2025, day 4

--- src/2025/day_04.cpp
+++ src/2025/day_04.cpp
@@ -25,58 +25,82 @@ namespace {
         };
     }
 
-    auto adjacent_cells(const point& loc, int wd, int hgt) {
+    auto adjacent_cells(const point& loc) {
         const static std::array<point, 8> directions = {{
             {-1,-1},{0,-1},{1,-1},
             {-1, 0},       {1, 0},
             {-1, 1},{0, 1},{1, 1}
         }};
         return directions | rv::transform(
-                [loc](auto&& dir)->point {
-                    return loc + dir;
-                }
-            ) | rv::filter(
-                [wd, hgt](auto&& pt) {
-                    return pt.x >= 0 && pt.y >= 0 && pt.x < wd && pt.y < hgt;
-                }
-            );
+            [loc](auto&& dir)->point {
+                return loc + dir;
+            }
+        );
     }
 
-    int neighbor_count(const point& loc, const grid& g) {
-        auto [wd, hgt] = bounds(g);
-        int count = 0;
-        for (auto adj : adjacent_cells(loc, wd, hgt)) {
-            count += ((g[adj.y][adj.x] == '@') ? 1 : 0);
-        }
-        return count;
+    int neighbor_count(const point& loc, const point_set& locs) {
+        return r::count_if(
+            adjacent_cells(loc),
+            [&](const auto& adj) {
+                return locs.contains(adj);
+            }
+        );
     }
 
-    int do_part_1(const grid& g) {
+    point_set to_point_set(const grid& g) {
         auto [wd, hgt] = bounds(g);
-        int count = 0;
+        point_set set;
         for (int y = 0; y < hgt; ++y) {
             for (int x = 0; x < wd; ++x) {
-                if (g[y][x] != '@') {
-                    continue;
+                if (g[y][x] == '@') {
+                    set.insert({ x,y });
                 }
-                if (neighbor_count({ x,y }, g) < 4) {
-                    ++count;
-                } 
             }
         }
-        return count;
+        return set;
+    }
+
+    point_set do_one_generation(const point_set& locs) {
+        point_set next_gen;
+        for (const auto& loc : locs) {
+            if (neighbor_count(loc, locs) >= 4) {
+                next_gen.insert(loc);
+            }
+        }
+        return next_gen;
+    }
+
+    int do_part_1(const point_set& locs) {
+        auto new_locs = do_one_generation(locs);
+        return locs.size() - new_locs.size();
+    }
+
+    int do_part_2(const point_set& locs) {
+        int total_removed = 0;
+        auto curr_locs = locs;
+        int just_removed = -1;
+
+        while (just_removed != 0) {
+            auto new_locs = do_one_generation(curr_locs);
+            just_removed = curr_locs.size() - new_locs.size();
+            total_removed += just_removed;
+            curr_locs = new_locs;
+        }
+
+        return total_removed;
     }
 }
 
 void aoc::y2025::day_04(const std::string& title) {
 
-    auto inp = aoc::file_to_string_vector(
+    auto inp = to_point_set(
+        aoc::file_to_string_vector(
             aoc::input_path(2025, 4)
-        ); 
-
+        )
+    );
 
     std::println("--- Day 4: {} ---", title);
     std::println("  part 1: {}", do_part_1(inp) );
-    std::println("  part 2: {}", 0);
+    std::println("  part 2: {}", do_part_2(inp) );
     
 }
