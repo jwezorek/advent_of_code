11fc27e5b38fe4e9cd2fa571abc3fc470cee26ab
joe wezorek <jwezorek@gmail.com>
2025-12-07 05:43:22 -0800

    2025, day 7

--- src/2025/day_07.cpp
+++ src/2025/day_07.cpp
@@ -3,7 +3,7 @@
 #include "../util/vec2.h"
 #include "y2025.h"
 #include <filesystem>
-#include <functional>
+#include <stack>
 #include <print>
 #include <ranges>
 #include <unordered_set>
@@ -16,64 +16,135 @@ namespace rv = std::ranges::views;
 namespace {
 
     using point = aoc::vec2<int>;
-    using point_set = aoc::vec2_set<int>;
+    using point_map = aoc::vec2_map<int, int>;
+    using graph = std::vector<std::vector<int>>;
 
-    std::tuple<point, point_set> parse_input(const std::vector<std::string>& inp) {
+    point_map splitter_locations(const std::vector<std::string>& inp) {
         int wd = static_cast<int>(inp.front().size());
         int hgt = static_cast<int>(inp.size());
         point start = { -1,-1 }; 
-        point_set splitters;
+        point_map splitters;
+
+        int id = 0;
         for (auto y = 0; y < hgt; ++y) {
             for (int x = 0; x < wd; ++x) {
                 auto ch = inp[y][x];
                 if (ch == 'S') {
-                    start = { x,y };
+                    splitters[point{ x,y }] = id++;
                     continue;
                 }
                 if (ch == '^') {
-                    splitters.insert({ x,y });
+                    splitters[point{ x,y }] = id++;
                 }
             }
         }
-        return { start, splitters };
+
+        return splitters;
     }
 
-    int do_part_1(const point& start, const point_set& splitters) {
-        std::unordered_set<int> beams_xpos = { start.x };
-        int y = start.y;
-        int hgt = r::max(splitters | rv::transform([](auto&& p) {return p.y; })) + 1;
-        int count = 0;
+    std::optional<int> next_splitter(const point& loc, const point_map& loc_to_id, int hgt) {
+        auto u = loc;
+        while (u.y < hgt && !loc_to_id.contains(u)) {
+            u = u + point{ 0,1 };
+        }
+        if (loc_to_id.contains(u)) {
+            return loc_to_id.at(u);
+        }
+        return {};
+    }
 
-        while (y < hgt) {
-            std::unordered_set<int> new_beams;
-            for (auto x : beams_xpos) {
-                if (splitters.contains({ x, y })) {
-                    new_beams.insert( x - 1 );
-                    new_beams.insert( x + 1 );
-                    ++count;
-                } else {
-                    new_beams.insert( x );
-                }
+    graph build_graph(const std::vector<std::string>& inp) {
+        point_map loc_to_id = splitter_locations(inp);
+        int hgt = static_cast<int>(inp.size());
+
+        graph g(loc_to_id.size());
+        for (const auto& [loc, id] : loc_to_id) {
+            if (id == 0) {
+                auto next = next_splitter(loc + point{0,1}, loc_to_id, hgt);
+                g[id].push_back(*next);
+                continue;
             }
-            beams_xpos = new_beams;
-            ++y;
+            auto left = next_splitter(loc - point{ 1,0 }, loc_to_id, hgt);
+            if (left) {
+                g[id].push_back(*left);
+            }
+            auto right = next_splitter(loc + point{ 1,0 }, loc_to_id, hgt);
+            if (right) {
+                g[id].push_back(*right);
+            }
+        }
+
+        return g;
+    }
+
+    int count_reachable_splitters(const graph& g) {
+        std::stack<int> stack;
+        std::unordered_set<int> visited;
+
+        int count = 0;
+        stack.push(0);
+        while (!stack.empty()) {
+            auto curr = stack.top();
+            stack.pop();
+            
+            if (visited.contains(curr)) {
+                continue;
+            }
+            visited.insert(curr);
+
+            if (curr != 0) {
+                ++count;
+            }
+
+            for (const auto& adj : g[curr]) {
+                stack.push(adj);
+            }
         }
 
         return count;
     }
 
+    int64_t count_paths_aux(const graph& g, int u, std::unordered_map<int, int64_t>& memos) {
+
+        if (memos.contains(u)) {
+            return memos.at(u);
+        }
+
+        const auto& adj_list = g[u];
+        int n = adj_list.size();
+        int64_t sum = 0;
+
+        if (n == 0) {
+            sum = 2;
+        } else if (n == 1) {
+            int other_branch = (u == 0) ? 0 : 1;
+            sum = other_branch + count_paths_aux(g, adj_list[0], memos);
+        } else if (n == 2) {
+            sum = count_paths_aux(g, adj_list[0], memos) +
+                count_paths_aux(g, adj_list[1], memos);
+        }
+
+        memos[u] = sum;
+        return sum;
+    }
+
+    int64_t count_paths(const graph& g) {
+        std::unordered_map<int, int64_t> memos;
+        return count_paths_aux(g, 0, memos);
+    }
 }
+
 
 void aoc::y2025::day_07(const std::string& title) {
 
-    auto [start, splitters] = parse_input(
-            aoc::file_to_string_vector(
-                aoc::input_path(2025, 7)
-            )
-        );
+    auto g = build_graph(
+        aoc::file_to_string_vector(
+            aoc::input_path(2025, 7)
+        )
+    );
 
     std::println("--- Day 7: {} ---", title);
-    std::println("  part 1: {}", do_part_1(start, splitters));
-    std::println("  part 2: {}", 0);
+    std::println("  part 1: {}", count_reachable_splitters(g));
+    std::println("  part 2: {}", count_paths(g) );
     
 }
